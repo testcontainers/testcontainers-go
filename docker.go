fa4f2e93fa1c24ee75209be64ecf0811083a2693
Gregory Vander Schueren <gregory.vanderschueren@tessares.net>
2020-03-30 09:54:27 +0200

    docker:getGatewayIp: rewrite with netlink?
    Signed-off-by: Gregory Vander Schueren <gregory.vanderschueren@tessares.net>

--- docker.go
+++ docker.go
@@ -731,6 +731,7 @@ func inAContainer() bool {
 
 func getGatewayIp() (string, error) {
 	// see https://github.com/testcontainers/testcontainers-java/blob/3ad8d80e2484864e554744a4800a81f6b7982168/core/src/main/java/org/testcontainers/dockerclient/DockerClientConfigUtils.java#L27
+	// Replace this with netlink (linux only)? Or something like https://github.com/jackpal/gateway?
 	cmd := exec.Command("sh", "-c", "ip route|awk '/default/ { print $3 }'")
 	stdout, err := cmd.Output()
 	if err != nil {
