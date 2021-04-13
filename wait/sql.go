889a45b0f0041b569112ea624e1dc00e5ea9d883
Michiel Alexander Boekhoff <michielalexanderb@gmail.com>
2021-04-13 12:20:07 +0100

    Add waitForSqlWithHost implementation.
    This implementation mostly copies the prior waitForSql implementation,
    the difference being that the waitForSqlWithHost allows the user to
    construct a connection string using the host of the DB container. This
    is especially useful in a scenario like CI (where our tests are being
    executed in a container) or even remote Docker hosts.
    
    To preserve backwards-compatibility, and because of the way that
    `waitForSql` works, `waitForSqlWithHost` replicates the existing
    behaviour mostly. In a v1 of testcontainers-go, you would probably want
    to unify these two differing implementations.

--- wait/sql.go
+++ wait/sql.go
@@ -9,9 +9,23 @@ import (
 	"github.com/docker/go-connections/nat"
 )
 
+var _ Strategy = &waitForSqlWithHost{}
+
 //ForSQL constructs a new waitForSql strategy for the given driver
 func ForSQL(port nat.Port, driver string, url func(nat.Port) string) *waitForSql {
 	return &waitForSql{
+		Port:           port,
+		URL:            url,
+		Driver:         driver,
+		startupTimeout: defaultStartupTimeout(),
+		PollInterval:   defaultPollInterval(),
+	}
+}
+
+//ForSQLWithHost constructs a new waitForSqlWithHost strategy for the given driver.ForSQLWithHost
+//It is functionally identical to waitForSql, but the URL gives you access to the DB's host as well as the port.
+func ForSQLWithHost(port nat.Port, driver string, url func(nat.Port, string) string) *waitForSqlWithHost {
+	return &waitForSqlWithHost{
 		Port:           port,
 		URL:            url,
 		Driver:         driver,
@@ -62,6 +76,78 @@ func (w *waitForSql) WaitUntilReady(ctx context.Context, target StrategyTarget) 
 	}
 
 	db, err := sql.Open(w.Driver, w.URL(port))
+	if err != nil {
+		return fmt.Errorf("sql.Open: %v", err)
+	}
+	defer db.Close()
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case <-ticker.C:
+
+			if _, err := db.ExecContext(ctx, "SELECT 1"); err != nil {
+				continue
+			}
+			return nil
+		}
+	}
+}
+
+type waitForSqlWithHost struct {
+	URL            func(port nat.Port, host string) string
+	Driver         string
+	Port           nat.Port
+	startupTimeout time.Duration
+	PollInterval   time.Duration
+}
+
+//Timeout sets the maximum waiting time for the strategy after which it'll give up and return an error
+func (w *waitForSqlWithHost) Timeout(duration time.Duration) *waitForSqlWithHost {
+	w.startupTimeout = duration
+	return w
+}
+
+//WithPollInterval can be used to override the default polling interval
+func (w *waitForSqlWithHost) WithPollInterval(pollInterval time.Duration) *waitForSqlWithHost {
+	w.PollInterval = pollInterval
+	return w
+}
+
+//WaitUntilReady repeatedly tries to run "SELECT 1" query on the given port using sql and driver.
+//If it doesn't succeed before the context timeout, it will return an error.
+func (w *waitForSqlWithHost) WaitUntilReady(ctx context.Context, target StrategyTarget) (err error) {
+	ctx, cancel := context.WithTimeout(ctx, w.startupTimeout)
+	defer cancel()
+
+	ticker := time.NewTicker(w.PollInterval)
+	defer ticker.Stop()
+
+	var port nat.Port
+	port, err = target.MappedPort(ctx, w.Port)
+
+	for port == "" {
+		select {
+		case <-ctx.Done():
+			return fmt.Errorf("%s:%w", ctx.Err(), err)
+		case <-ticker.C:
+			port, err = target.MappedPort(ctx, w.Port)
+		}
+	}
+
+	var host string
+	host, err = target.Host(ctx)
+
+	for host == "" {
+		select {
+		case <-ctx.Done():
+			return fmt.Errorf("%s:%w", ctx.Err(), err)
+		case <-ticker.C:
+			host, err = target.Host(ctx)
+		}
+	}
+
+	db, err := sql.Open(w.Driver, w.URL(port, host))
 	if err != nil {
 		return fmt.Errorf("sql.Open: %v", err)
 	}
