77129bc82ef67f6b229e5473b984712035bd1058
Peter Kurfer <peter.kurfer@googlemail.com>
2021-12-24 11:14:26 +0100

    Add TestLogger helper

--- logger.go
+++ logger.go
@@ -3,6 +3,7 @@ package testcontainers
 import (
 	"log"
 	"os"
+	"testing"
 )
 
 // Logger is the default log instance
@@ -11,4 +12,16 @@ var Logger Logging = log.New(os.Stderr, "", log.LstdFlags)
 // Logging defines the Logger interface
 type Logging interface {
 	Printf(format string, v ...interface{})
+}
+
+func TestLogger(tb testing.TB) Logging {
+	return testLogger{TB: tb}
+}
+
+type testLogger struct {
+	testing.TB
+}
+
+func (t testLogger) Printf(format string, v ...interface{}) {
+	t.Logf(format, v...)
 }
