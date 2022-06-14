2f903143ba2e518d7355531f13bb0bcb8cc3cae7
Denis Gukov <denguk@gmail.com>
2022-06-14 18:54:37 +0500

    feat(compose): implement base functionality of containerised compose

--- containerised_compose.go
+++ containerised_compose.go
@@ -2,6 +2,11 @@ package testcontainers
 
 import (
 	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
 )
 
 type ContainerisedDockerComposeOptions struct {
@@ -13,35 +18,85 @@ type ContainerisedDockerCompose struct {
 	ContainerisedDockerComposeOptions
 	Env     map[string]string
 	Context context.Context
+	// Pwd is a path to the directory where compose files located.
+	Pwd string
+	// ContainerPwd is a path to the directory in the container where Pwd mounted.
+	ContainerPwd string
 }
 
 func (c *ContainerisedDockerCompose) Invoke() ExecError {
 	req := ContainerRequest{
-		Image:      "docker/compose:1.29.2",
-		Env:        c.Env,
-		Mounts:     Mounts(BindMount("/var/run/docker.sock", "/var/run/docker.sock")),
-		SkipReaper: true,
-		AutoRemove: true,
+		Image: "docker/compose:1.29.2",
+		Env:   c.Env,
+		Mounts: Mounts(
+			BindMount(
+				coalesce(os.Getenv("TESTCONTAINERS_DOCKER_SOCKET_OVERRIDE"), "/var/run/docker.sock"),
+				"/var/run/docker.sock",
+			),
+			BindMount(c.Pwd, ContainerMountTarget(c.ContainerPwd)),
+		),
+		Cmd: []string{"docker-compose"},
 	}
 
-	_, err := c.Provider.RunContainer(c.Context, req)
+	container, err := c.Provider.RunContainer(c.Context, req)
 	if err != nil {
-		return ExecError{}
+		return ExecError{
+			Command: req.Cmd,
+			Error:   err,
+		}
 	}
 
-	// TODO: waiting until finished
+	state, err := container.State(c.Context)
 
-	return ExecError{}
+	for err == nil && state.FinishedAt == "" {
+		time.Sleep(60000)
+		state, err = container.State(c.Context)
+	}
+
+	if state.Status == "died" {
+		err = fmt.Errorf(state.Error)
+	}
+
+	if state.Status == "exited" && state.ExitCode != 0 {
+		err = fmt.Errorf(state.Error)
+	}
+
+	return ExecError{
+		Command: req.Cmd,
+		Error:   err,
+	}
 }
 
 func NewContainerisedDockerCompose(filePaths []string, identifier string, options ContainerisedDockerComposeOptions) *ContainerisedDockerCompose {
-	composeFile := ""
+	containerPwd := "/app"
+	pwd, err := filepath.Abs(filepath.Dir(filePaths[0]))
+
+	if err != nil {
+		return nil
+	}
+
+	var composeFiles []string
+
+	for _, p := range filePaths {
+		absPath, err := filepath.Abs(p)
+		if err != nil {
+			return nil
+		}
+
+		if !strings.HasPrefix(absPath, pwd+string(filepath.Separator)) {
+			return nil
+		}
+
+		composeFiles = append(composeFiles, containerPwd+"/"+filepath.ToSlash(absPath[len(pwd)+1:]))
+	}
 
 	return &ContainerisedDockerCompose{
+		Pwd:                               pwd,
+		ContainerPwd:                      containerPwd,
 		ContainerisedDockerComposeOptions: options,
 		Env: map[string]string{
-			"ENV_PROJECT_NAME": identifier,
-			"ENV_COMPOSE_FILE": composeFile,
+			"COMPOSE_PROJECT_NAME": identifier,
+			"COMPOSE_FILE":         strings.Join(composeFiles, ":"),
 		},
 	}
 }
--- containerised_compose_test.go
+++ containerised_compose_test.go
@@ -1,9 +1,134 @@
 package testcontainers
 
-import "testing"
+import (
+	"context"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/go-connections/nat"
+	"io"
+	"testing"
+	"time"
+)
+
+type FakeContainerProvider struct {
+}
+
+func (p *FakeContainerProvider) CreateContainer(ctx context.Context, request ContainerRequest) (Container, error) {
+	panic("not implemented")
+}
+
+func (p *FakeContainerProvider) Health(ctx context.Context) error {
+	panic("not implemented")
+}
+
+type FakeContainer struct {
+}
+
+func (f FakeContainer) GetContainerID() string {
+	panic("not implemented")
+}
+
+func (f FakeContainer) Endpoint(ctx context.Context, s string) (string, error) {
+	panic("not implemented")
+}
+
+func (f FakeContainer) PortEndpoint(ctx context.Context, port nat.Port, s string) (string, error) {
+	panic("not implemented")
+}
+
+func (f FakeContainer) Host(ctx context.Context) (string, error) {
+	panic("not implemented")
+}
+
+func (f FakeContainer) MappedPort(ctx context.Context, port nat.Port) (nat.Port, error) {
+	panic("not implemented")
+}
+
+func (f FakeContainer) Ports(ctx context.Context) (nat.PortMap, error) {
+	panic("not implemented")
+}
+
+func (f FakeContainer) SessionID() string {
+	panic("not implemented")
+}
+
+func (f FakeContainer) Start(ctx context.Context) error {
+	panic("not implemented")
+}
+
+func (f FakeContainer) Stop(ctx context.Context, duration *time.Duration) error {
+	panic("not implemented")
+}
+
+func (f FakeContainer) Terminate(ctx context.Context) error {
+	panic("not implemented")
+}
+
+func (f FakeContainer) Logs(ctx context.Context) (io.ReadCloser, error) {
+	panic("not implemented")
+}
+
+func (f FakeContainer) FollowOutput(consumer LogConsumer) {
+	panic("not implemented")
+}
+
+func (f FakeContainer) StartLogProducer(ctx context.Context) error {
+	panic("not implemented")
+}
+
+func (f FakeContainer) StopLogProducer() error {
+	panic("not implemented")
+}
+
+func (f FakeContainer) Name(ctx context.Context) (string, error) {
+	panic("not implemented")
+}
+
+func (f FakeContainer) State(ctx context.Context) (*types.ContainerState, error) {
+	return &types.ContainerState{
+		Status:     "exited",
+		FinishedAt: "2022-06-12",
+	}, nil
+}
+
+func (f FakeContainer) Networks(ctx context.Context) ([]string, error) {
+	panic("not implemented")
+}
+
+func (f FakeContainer) NetworkAliases(ctx context.Context) (map[string][]string, error) {
+	panic("not implemented")
+}
+
+func (f FakeContainer) Exec(ctx context.Context, cmd []string) (int, io.Reader, error) {
+	panic("not implemented")
+}
+
+func (f FakeContainer) ContainerIP(ctx context.Context) (string, error) {
+	panic("not implemented")
+}
+
+func (f FakeContainer) CopyToContainer(ctx context.Context, fileContent []byte, containerFilePath string, fileMode int64) error {
+	panic("not implemented")
+}
+
+func (f FakeContainer) CopyFileToContainer(ctx context.Context, hostFilePath string, containerFilePath string, fileMode int64) error {
+	panic("not implemented")
+}
+
+func (f FakeContainer) CopyFileFromContainer(ctx context.Context, filePath string) (io.ReadCloser, error) {
+	panic("not implemented")
+}
+
+func (p *FakeContainerProvider) RunContainer(context.Context, ContainerRequest) (Container, error) {
+	return FakeContainer{}, nil
+}
 
 func Test_NewContainerisedDockerCompose(t *testing.T) {
-	compose := NewContainerisedDockerCompose([]string{}, "", ContainerisedDockerComposeOptions{})
+	ctx := context.Background()
+
+	compose := NewContainerisedDockerCompose([]string{"docker-compose.yml"}, "test", ContainerisedDockerComposeOptions{
+		Provider: &FakeContainerProvider{},
+		Context:  ctx,
+	})
 
 	res := compose.Invoke()
 
