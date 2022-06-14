bdc5c88d8daf1b3f142638b63d808cb03c3bd083
Vladimir Stepanov <v.stepanov@redmadrobot.com>
2022-06-14 23:42:11 +0600

    add 'docker ps' output

--- scripts/check_environment.sh
+++ scripts/check_environment.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-
+docker_out="/tmp/dout.txt"
 containers=$(docker ps --format "{{.ID}}" | wc -l)
 
 if [ "$containers" -eq "0" ]; then
    exit 0
 fi
 
+echo "$(docker ps)"
 echo "Number of containers are still running:" "$containers"
-
 exit 255;
