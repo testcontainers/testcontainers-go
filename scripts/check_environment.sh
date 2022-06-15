44e09ea08e04afea46023c292879c444409eb138
Vladimir Stepanov <v.stepanov@redmadrobot.com>
2022-06-15 08:51:07 +0600

    add waiting loop

--- scripts/check_environment.sh
+++ scripts/check_environment.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
-containers=$(docker ps --format "{{.ID}}" | wc -l)
+i=0
+while [ $i -ne 15 ]
+do
+        containers=$(docker ps --format "{{.ID}}" | wc -l)
 
-if [ "$containers" -eq "0" ]; then
-   exit 0
-fi
+        if [ "$containers" -eq "0" ]; then
+           exit 0
+        fi
+        sleep 2
+done
 
 docker ps
 echo "Number of containers are still running:" "$containers"
