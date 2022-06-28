89fd84a5843f5dde7d77d480f2f66f2cbf357701
Vladimir Stepanov <v.stepanov@redmadrobot.com>
2022-06-28 19:37:26 +0600

    test: add new test for checking containers running (all errors)

--- parallel_test.go
+++ parallel_test.go
@@ -13,32 +13,6 @@ func TestParallelContainers(t *testing.T) {
 		expErrors int
 	}{
 		{
-			name: "running two containers (success)",
-			reqs: ParallelContainerRequest{
-				{
-					ContainerRequest: ContainerRequest{
-
-						Image: "nginx",
-						ExposedPorts: []string{
-							"10080/tcp",
-						},
-					},
-					Started: true,
-				},
-				{
-					ContainerRequest: ContainerRequest{
-
-						Image: "nginx",
-						ExposedPorts: []string{
-							"10081/tcp",
-						},
-					},
-					Started: true,
-				},
-			},
-			resLen: 2,
-		},
-		{
 			name: "running two containers (one error)",
 			reqs: ParallelContainerRequest{
 				{
@@ -64,6 +38,60 @@ func TestParallelContainers(t *testing.T) {
 			},
 			resLen:    1,
 			expErrors: 1,
+		},
+		{
+			name: "running two containers (all errors)",
+			reqs: ParallelContainerRequest{
+				{
+					ContainerRequest: ContainerRequest{
+
+						Image: "bad bad bad",
+						ExposedPorts: []string{
+							"10081/tcp",
+						},
+					},
+					Started: true,
+				},
+				{
+					ContainerRequest: ContainerRequest{
+
+						Image: "bad bad bad",
+						ExposedPorts: []string{
+							"10081/tcp",
+						},
+					},
+					Started: true,
+				},
+			},
+			resLen:    0,
+			expErrors: 2,
+		},
+		{
+			name: "running two containers (success)",
+			reqs: ParallelContainerRequest{
+				{
+					ContainerRequest: ContainerRequest{
+
+						Image: "nginx",
+						ExposedPorts: []string{
+							"10080/tcp",
+						},
+					},
+					Started: true,
+				},
+				{
+					ContainerRequest: ContainerRequest{
+
+						Image: "nginx",
+						ExposedPorts: []string{
+							"10081/tcp",
+						},
+					},
+					Started: true,
+				},
+			},
+			resLen:    2,
+			expErrors: 0,
 		},
 	}
 
