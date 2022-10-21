786954c7be036e282e86adabed2e86c49e10a32f
Manuel de la Peña <mdelapenya@gmail.com>
2022-10-21 19:38:12 +0200

    fix: check if the container request if for the reaper (#574)

--- docker.go
+++ docker.go
@@ -943,7 +943,9 @@ func (p *DockerProvider) CreateContainer(ctx context.Context, req ContainerReque
 	sessionID := sessionID()
 
 	var termSignal chan bool
-	if !req.SkipReaper {
+	// the reaper does not need to start a reaper for itself
+	isReaperContainer := strings.EqualFold(req.Image, reaperImage(req.ReaperImage))
+	if !req.SkipReaper && !isReaperContainer {
 		r, err := NewReaper(context.WithValue(ctx, dockerHostContextKey, p.host), sessionID.String(), p, req.ReaperImage)
 		if err != nil {
 			return nil, fmt.Errorf("%w: creating reaper failed", err)
@@ -957,7 +959,7 @@ func (p *DockerProvider) CreateContainer(ctx context.Context, req ContainerReque
 				req.Labels[k] = v
 			}
 		}
-	} else {
+	} else if !isReaperContainer {
 		p.printReaperBanner("container")
 	}
 
