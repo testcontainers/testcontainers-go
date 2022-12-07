e10f87f2fac1e85523d78e1abf6a3d6b21236f04
Manuel de la Peña <mdelapenya@gmail.com>
2022-12-07 18:22:47 +0100

    chore: more tests

--- examples/postgres/postgres.go
+++ examples/postgres/postgres.go
@@ -2,8 +2,10 @@ package postgres
 
 import (
 	"context"
+	"time"
 
 	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
 )
 
 // postgresContainer represents the postgres container type used in the module
@@ -11,11 +13,41 @@ type postgresContainer struct {
 	testcontainers.Container
 }
 
-// setupPostgres creates an instance of the postgres container type
-func setupPostgres(ctx context.Context) (*postgresContainer, error) {
-	req := testcontainers.ContainerRequest{
-		Image: "postgres:11-alpine",
+type postgresContainerOption func(req *testcontainers.ContainerRequest)
+
+func WithWaitStrategy(strategies ...wait.Strategy) func(req *testcontainers.ContainerRequest) {
+	return func(req *testcontainers.ContainerRequest) {
+		req.WaitingFor = wait.ForAll(strategies...).WithDeadline(1 * time.Minute)
 	}
+}
+
+func WithPort(port string) func(req *testcontainers.ContainerRequest) {
+	return func(req *testcontainers.ContainerRequest) {
+		req.ExposedPorts = append(req.ExposedPorts, port)
+	}
+}
+
+func WithInitialDatabase(user string, password string, dbName string) func(req *testcontainers.ContainerRequest) {
+	return func(req *testcontainers.ContainerRequest) {
+		req.Env["POSTGRES_USER"] = user
+		req.Env["POSTGRES_PASSWORD"] = password
+		req.Env["POSTGRES_DB"] = dbName
+	}
+}
+
+// setupPostgres creates an instance of the postgres container type
+func setupPostgres(ctx context.Context, opts ...postgresContainerOption) (*postgresContainer, error) {
+	req := testcontainers.ContainerRequest{
+		Image:        "postgres:11-alpine",
+		Env:          map[string]string{},
+		ExposedPorts: []string{},
+		Cmd:          []string{"postgres", "-c", "fsync=off"},
+	}
+
+	for _, opt := range opts {
+		opt(&req)
+	}
+
 	container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
 		ContainerRequest: req,
 		Started:          true,
