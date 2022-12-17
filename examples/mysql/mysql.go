ae10610e9a131ffd9cfd24f863b14ec9a963c5e1
Manuel de la Peña <mdelapenya@gmail.com>
2022-12-17 09:16:34 +0100

    chore: add test for mysql example module

--- examples/mysql/mysql.go
+++ examples/mysql/mysql.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
 )
 
 // mysqlContainer represents the mysql container type used in the module
@@ -14,7 +15,16 @@ type mysqlContainer struct {
 // setupMysql creates an instance of the mysql container type
 func setupMysql(ctx context.Context) (*mysqlContainer, error) {
 	req := testcontainers.ContainerRequest{
-		Image: "mysql:8",
+		Image:        "mysql:8",
+		ExposedPorts: []string{"3306/tcp", "33060/tcp"},
+		Env: map[string]string{
+			"MYSQL_ROOT_PASSWORD": "password",
+			"MYSQL_DATABASE":      "database",
+		},
+		WaitingFor: wait.ForAll(
+			wait.ForLog("port: 3306  MySQL Community Server - GPL"),
+			wait.ForListeningPort("3306/tcp"),
+		),
 	}
 	container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
 		ContainerRequest: req,
