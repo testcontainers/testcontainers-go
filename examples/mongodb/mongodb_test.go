bf967072153a4c1db01bb84fbf59dd30eb94d781
ravilushqa <ravilushqa@users.noreply.github.com>
2023-01-04 21:41:48 +0400

    MongoDB example (#723)
    * init
    
    * mongodb implementation
    
    * improve naming

---
+++ examples/mongodb/mongodb_test.go
@@ -0,0 +1,50 @@
+package mongodb
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func TestMongoDB(t *testing.T) {
+	ctx := context.Background()
+
+	container, err := setupMongoDB(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Clean up the container after the test is complete
+	t.Cleanup(func() {
+		if err := container.Terminate(ctx); err != nil {
+			t.Fatalf("failed to terminate container: %s", err)
+		}
+	})
+
+	// perform assertions
+
+	host, _ := container.Host(ctx)
+
+	p, _ := container.MappedPort(ctx, "27017/tcp")
+	port := p.Int()
+
+	connectionString := fmt.Sprintf("mongodb://%s:%d", host, port)
+
+	mongoClient, err := mongo.NewClient(options.Client().ApplyURI(connectionString))
+	if err != nil {
+		t.Fatal(fmt.Errorf("error creating mongo client: %w", err))
+	}
+
+	err = mongoClient.Connect(ctx)
+	if err != nil {
+		t.Fatal(fmt.Errorf("error connecting to mongo: %w", err))
+	}
+
+	err = mongoClient.Ping(ctx, nil)
+	if err != nil {
+		t.Fatal(fmt.Errorf("error pinging mongo: %w", err))
+	}
+}
---
+++ examples/mongodb/tools/tools.go
@@ -0,0 +1,11 @@
+//go:build tools
+// +build tools
+
+// This package contains the tool dependencies of the mongodb example.
+
+package tools
+
+import (
+	// Register gotestsum for pinning version
+	_ "gotest.tools/gotestsum"
+)
