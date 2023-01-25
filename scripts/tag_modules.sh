a7da93c2ae4db107abf94160fc5ab5de755ab48e
Manuel de la Peña <mdelapenya@gmail.com>
2023-01-25 01:24:26 +0100

    chore: automate the creation of tags for all modules

---
+++ scripts/tag_modules.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+readonly CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+readonly ROOT_DIR="$(dirname "$CURRENT_DIR")"
+
+readonly tag="${1}"
+
+directories=(examples modules)
+
+for directory in "${directories[@]}"
+do
+  cd "${ROOT_DIR}/${directory}"
+
+  ls -d */ | grep -v "_template" | while read -r module; do
+    module=${module%?} # remove trailing slash
+    module_tag="${directory}/${module}/${tag}" # e.g. modules/mongodb/0.0.1
+    git tag -d "${module_tag}" | true # do not fail if tag does not exist
+    git tag "${module_tag}"
+  done
+done
