5461f69014292db49d6b8b05dae1b97456672900
Manuel de la Peña <mdelapenya@gmail.com>
2023-01-30 22:37:25 +0100

    chore: support defining legacy mode

--- examples/localstack/localstack.go
+++ examples/localstack/localstack.go
@@ -3,19 +3,57 @@ package localstack
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/internal/testcontainersdocker"
 	"github.com/testcontainers/testcontainers-go/wait"
+	"golang.org/x/mod/semver"
 )
+
+const defaultVersion = "0.11.2"
 
 // localStackContainer represents the LocalStack container type used in the module
 type localStackContainer struct {
 	testcontainers.Container
+	legacyMode bool
+}
+
+func runInLegacyMode(version string) bool {
+	if version == "latest" {
+		return false
+	}
+
+	if !strings.HasPrefix(version, "v") {
+		version = fmt.Sprintf("v%s", version)
+	}
+
+	if semver.IsValid(version) {
+		return semver.Compare(version, "v0.11") < 0
+	}
+
+	fmt.Printf("Version %s is not a semantic version, LocalStack will run in legacy mode.\n", version)
+	fmt.Printf("Consider using \"setupLocalStack(context context.Context, version string, legacyMode bool)\" constructor if you want to disable legacy mode.")
+	return true
 }
 
 // setupLocalStack creates an instance of the LocalStack container type
-func setupLocalStack(ctx context.Context) (*localStackContainer, error) {
+func setupLocalStack(ctx context.Context, version string, legacyMode bool) (*localStackContainer, error) {
+	if version == "" {
+		version = defaultVersion
+	}
+
+	/*
+		Do not run in legacy mode when the version is a valid semver version greater than the v0.11 and legacyMode is false
+			| runInLegacyMode | legacyMode | result |
+			|-----------------|------------|--------|
+			| false           | false      | false  |
+			| false           | true       | true   |
+			| true            | false      | true   |
+			| true            | true       | true   |
+	*/
+	legacyMode = !runInLegacyMode(version) && !legacyMode
+
 	req := testcontainers.ContainerRequest{
 		Image:      "localstack/localstack:0.11.2",
 		Binds:      []string{fmt.Sprintf("%s:/var/run/docker.sock", testcontainersdocker.ExtractDockerHost(ctx))},
@@ -29,5 +67,5 @@ func setupLocalStack(ctx context.Context) (*localStackContainer, error) {
 		return nil, err
 	}
 
-	return &localStackContainer{Container: container}, nil
+	return &localStackContainer{Container: container, legacyMode: legacyMode}, nil
 }
--- examples/localstack/localstack_test.go
+++ examples/localstack/localstack_test.go
@@ -5,10 +5,34 @@ import (
 	"testing"
 )
 
+func TestRunInLegacyMode(t *testing.T) {
+	tests := []struct {
+		version string
+		want    bool
+	}{
+		{"foo", true},
+		{"latest", false},
+		{"0.10.0", true},
+		{"0.11", false},
+		{"0.11.2", false},
+		{"0.12", false},
+		{"1.0", false},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.version, func(t *testing.T) {
+			got := runInLegacyMode(tt.version)
+			if got != tt.want {
+				t.Errorf("runInLegacyMode() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
 func TestLocalStack(t *testing.T) {
 	ctx := context.Background()
 
-	container, err := setupLocalStack(ctx)
+	container, err := setupLocalStack(ctx, defaultVersion, false)
 	if err != nil {
 		t.Fatal(err)
 	}
