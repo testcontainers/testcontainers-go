4a435a30b63935e583691327146f82433ab97721
Manuel de la Peña <mdelapenya@gmail.com>
2023-02-10 17:18:14 +0100

    chore: move tests

--- modules/localstack/localstack_test.go
+++ modules/localstack/localstack_test.go
@@ -81,6 +81,50 @@ func TestIsLegacyMode(t *testing.T) {
 	}
 }
 
+func TestStart(t *testing.T) {
+	// forceLegacyMode {
+	ctx := context.Background()
+
+	container, err := StartContainer(
+		ctx,
+		OverrideContainerRequest(testcontainers.ContainerRequest{
+			Image: "localstack/localstack:0.12.0",
+		}),
+		WithServices(S3, SQS, Kinesis),
+	)
+	// }
+
+	t.Run("multiple services should be exposed using the same port", func(t *testing.T) {
+		require.Nil(t, err)
+		assert.NotNil(t, container)
+
+		rawPorts, err := container.Ports(ctx)
+		require.Nil(t, err)
+
+		ports := 0
+		// only one port is exposed among all the ports in the container
+		for _, v := range rawPorts {
+			if len(v) > 0 {
+				ports++
+			}
+		}
+
+		assert.Equal(t, 1, ports) // a single port is exposed
+
+		s3Endpoint, err := container.ServicePort(ctx, S3)
+		require.Nil(t, err)
+
+		sqsEndpoint, err := container.ServicePort(ctx, SQS)
+		require.Nil(t, err)
+
+		kinesisEndpoint, err := container.ServicePort(ctx, Kinesis)
+		require.Nil(t, err)
+
+		assert.Equal(t, sqsEndpoint, s3Endpoint)
+		assert.Equal(t, sqsEndpoint, kinesisEndpoint)
+	})
+}
+
 func TestStartWithoutOverride(t *testing.T) {
 	// noopOverrideContainerRequest {
 	ctx := context.Background()
