2832898815c9e4b1bb4f04a0cdb4237bec8966d6
Manuel de la Peña <mdelapenya@gmail.com>
2023-02-10 09:02:22 +0100

    chore: support pushing the new module versions to pkg.go.dev

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,6 +3,7 @@
 readonly CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 readonly ROOT_DIR="$(dirname "$CURRENT_DIR")"
 
+readonly repository="github.com/testcontainers/testcontainers-go"
 readonly tag="${1}"
 
 git tag -d "${tag}" | true # do not fail if tag does not exist
@@ -15,9 +16,20 @@ do
   cd "${ROOT_DIR}/${directory}"
 
   ls -d */ | grep -v "_template" | while read -r module; do
-    module=${module%?} # remove trailing slash
-    module_tag="${directory}/${module}/${tag}" # e.g. modules/mongodb/0.0.1
+    module="${module%?}" # remove trailing slash
+    module_tag="${directory}/${module}/${tag}" # e.g. modules/mongodb/v0.0.1
     git tag -d "${module_tag}" | true # do not fail if tag does not exist
     git tag "${module_tag}"
   done
+done
+
+git push --tags
+
+curl "https://proxy.golang.org/${repository}/@v/${tag}" # e.g. github.com/testcontainers/testcontainers-go/v0.0.1
+
+for directory in "${directories[@]}"
+do
+  module="${module%?}" # remove trailing slash
+  module_path="${repository}/${directory}/${module}/"
+  curl "https://proxy.golang.org/${module_path}/@v/${tag}" # e.g. # e.g. github.com/testcontainers/testcontainers-go/modules/mongodb/v0.0.1
 done
