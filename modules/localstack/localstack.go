8bba30b6cf58b306919a26b33701c2b43979260d
Manuel de la Peña <mdelapenya@gmail.com>
2023-02-14 10:46:48 +0100

    chore: bump to latest version

--- modules/localstack/localstack.go
+++ modules/localstack/localstack.go
@@ -14,7 +14,7 @@ import (
 
 const defaultPort = 4566
 const defaultRegion = "us-east-1"
-const defaultVersion = "1.3.1"
+const defaultVersion = "1.4.0"
 const hostnameExternalEnvVar = "HOSTNAME_EXTERNAL"
 
 const defaultAccessKeyID = "accesskey"
