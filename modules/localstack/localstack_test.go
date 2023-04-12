3925d4c1b09012db99b1fee86a0599c19af7730a
Eddú Meléndez Gonzales <eddu.melendez@gmail.com>
2023-04-12 09:39:28 -0500

    Add support for LocalStack v2 (#994)
    * Add support for LocalStack v2
    
    `HOSTNAME_EXTERNAL` env var is deprecated and will be replaced by
    `LOCALSTACK_HOST` in the upcoming v2.
    
    * Update image
    
    * Update modules/localstack/localstack.go
    
    Co-authored-by: Manuel de la Peña <mdelapenya@gmail.com>
    
    * Rename network
    
    * Use wait.ForExec
    
    ---------
    
    Co-authored-by: Manuel de la Peña <mdelapenya@gmail.com>

--- modules/localstack/localstack_test.go
+++ modules/localstack/localstack_test.go
@@ -3,7 +3,11 @@ package localstack
 import (
 	"context"
 	"fmt"
+	"github.com/testcontainers/testcontainers-go/wait"
+	"io"
+	"strings"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -20,52 +24,60 @@ func generateContainerRequest() *LocalStackContainerRequest {
 }
 
 func TestConfigureDockerHost(t *testing.T) {
+	tests := []struct {
+		envVar string
+	}{
+		{hostnameExternalEnvVar},
+		{localstackHostEnvVar},
+	}
 
-	t.Run("HOSTNAME_EXTERNAL variable is passed as part of the request", func(t *testing.T) {
-		req := generateContainerRequest()
+	for _, tt := range tests {
+		t.Run("HOSTNAME_EXTERNAL variable is passed as part of the request", func(t *testing.T) {
+			req := generateContainerRequest()
 
-		req.Env[hostnameExternalEnvVar] = "foo"
+			req.Env[tt.envVar] = "foo"
 
-		reason, err := configureDockerHost(req)
-		assert.Nil(t, err)
-		assert.Equal(t, "explicitly as environment variable", reason)
-	})
+			reason, err := configureDockerHost(req, tt.envVar)
+			assert.Nil(t, err)
+			assert.Equal(t, "explicitly as environment variable", reason)
+		})
 
-	t.Run("HOSTNAME_EXTERNAL matches the last network alias on a container with non-default network", func(t *testing.T) {
-		req := generateContainerRequest()
+		t.Run("HOSTNAME_EXTERNAL matches the last network alias on a container with non-default network", func(t *testing.T) {
+			req := generateContainerRequest()
 
-		req.Networks = []string{"foo", "bar", "baaz"}
-		req.NetworkAliases = map[string][]string{
-			"foo":  {"foo0", "foo1", "foo2", "foo3"},
-			"bar":  {"bar0", "bar1", "bar2", "bar3"},
-			"baaz": {"baaz0", "baaz1", "baaz2", "baaz3"},
-		}
+			req.Networks = []string{"foo", "bar", "baaz"}
+			req.NetworkAliases = map[string][]string{
+				"foo":  {"foo0", "foo1", "foo2", "foo3"},
+				"bar":  {"bar0", "bar1", "bar2", "bar3"},
+				"baaz": {"baaz0", "baaz1", "baaz2", "baaz3"},
+			}
 
-		reason, err := configureDockerHost(req)
-		assert.Nil(t, err)
-		assert.Equal(t, "to match last network alias on container with non-default network", reason)
-		assert.Equal(t, "foo3", req.Env[hostnameExternalEnvVar])
-	})
+			reason, err := configureDockerHost(req, tt.envVar)
+			assert.Nil(t, err)
+			assert.Equal(t, "to match last network alias on container with non-default network", reason)
+			assert.Equal(t, "foo3", req.Env[tt.envVar])
+		})
 
-	t.Run("HOSTNAME_EXTERNAL matches the daemon host because there are no aliases", func(t *testing.T) {
-		dockerProvider, err := testcontainers.NewDockerProvider()
-		assert.Nil(t, err)
-		defer dockerProvider.Close()
+		t.Run("HOSTNAME_EXTERNAL matches the daemon host because there are no aliases", func(t *testing.T) {
+			dockerProvider, err := testcontainers.NewDockerProvider()
+			assert.Nil(t, err)
+			defer dockerProvider.Close()
 
-		// because the daemon host could be a remote one, we need to get it from the provider
-		expectedDaemonHost, err := dockerProvider.DaemonHost(context.Background())
-		assert.Nil(t, err)
+			// because the daemon host could be a remote one, we need to get it from the provider
+			expectedDaemonHost, err := dockerProvider.DaemonHost(context.Background())
+			assert.Nil(t, err)
 
-		req := generateContainerRequest()
+			req := generateContainerRequest()
 
-		req.Networks = []string{"foo", "bar", "baaz"}
-		req.NetworkAliases = map[string][]string{}
+			req.Networks = []string{"foo", "bar", "baaz"}
+			req.NetworkAliases = map[string][]string{}
 
-		reason, err := configureDockerHost(req)
-		assert.Nil(t, err)
-		assert.Equal(t, "to match host-routable address for container", reason)
-		assert.Equal(t, expectedDaemonHost, req.Env[hostnameExternalEnvVar])
-	})
+			reason, err := configureDockerHost(req, tt.envVar)
+			assert.Nil(t, err)
+			assert.Equal(t, "to match host-routable address for container", reason)
+			assert.Equal(t, expectedDaemonHost, req.Env[tt.envVar])
+		})
+	}
 }
 
 func TestIsLegacyMode(t *testing.T) {
@@ -164,4 +176,55 @@ func TestStartWithNetwork(t *testing.T) {
 	require.Nil(t, err)
 	require.Equal(t, 1, len(networks))
 	require.Equal(t, "localstack-network", networks[0])
+}
+
+func TestStartV2WithNetwork(t *testing.T) {
+	ctx := context.Background()
+
+	nw, err := testcontainers.GenericNetwork(ctx, testcontainers.GenericNetworkRequest{
+		NetworkRequest: testcontainers.NetworkRequest{
+			Name: "localstack-network-v2",
+		},
+	})
+	require.Nil(t, err)
+	assert.NotNil(t, nw)
+
+	localstack, err := StartContainer(
+		ctx,
+		OverrideContainerRequest(testcontainers.ContainerRequest{
+			Image:          "localstack/localstack:2.0.0",
+			Networks:       []string{"localstack-network-v2"},
+			NetworkAliases: map[string][]string{"localstack-network-v2": {"localstack"}},
+		}),
+	)
+	require.Nil(t, err)
+	assert.NotNil(t, localstack)
+
+	cli, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: testcontainers.ContainerRequest{
+			Image:      "amazon/aws-cli:2.7.27",
+			Networks:   []string{"localstack-network-v2"},
+			Entrypoint: []string{"tail"},
+			Cmd:        []string{"-f", "/dev/null"},
+			Env: map[string]string{
+				"AWS_ACCESS_KEY_ID":     "accesskey",
+				"AWS_SECRET_ACCESS_KEY": "secretkey",
+				"AWS_REGION":            "eu-west-1",
+			},
+			WaitingFor: wait.ForExec([]string{"/usr/local/bin/aws", "sqs", "create-queue", "--queue-name", "baz", "--region", "eu-west-1",
+				"--endpoint-url", "http://localstack:4566", "--no-verify-ssl"}).
+				WithStartupTimeout(time.Second * 10).
+				WithExitCodeMatcher(func(exitCode int) bool {
+					return exitCode == 0
+				}).
+				WithResponseMatcher(func(r io.Reader) bool {
+					respBytes, _ := io.ReadAll(r)
+					resp := string(respBytes)
+					return strings.Contains(resp, "http://localstack:4566")
+				}),
+		},
+		Started: true,
+	})
+	require.Nil(t, err)
+	assert.NotNil(t, cli)
 }
