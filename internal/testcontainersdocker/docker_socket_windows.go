54ab52c39c32ed17725e52a1988a36988396885f
Manuel de la Peña <mdelapenya@gmail.com>
2023-05-04 16:35:19 +0200

    chore: convert internal constants into vars for testability

--- internal/testcontainersdocker/docker_socket_windows.go
+++ internal/testcontainersdocker/docker_socket_windows.go
@@ -1,7 +1,7 @@
 package testcontainersdocker
 
 // DockerSocketPath is the path to the docker socket under windows systems.
-const DockerSocketPath = "//./pipe/docker_engine"
+var DockerSocketPath = "//./pipe/docker_engine"
 
 // DockerSocketPathWithSchema is the path to the docker socket under windows systems with the npipe schema.
-const DockerSocketPathWithSchema = "npipe:////./pipe/docker_engine"
+var DockerSocketPathWithSchema = "npipe:////./pipe/docker_engine"
