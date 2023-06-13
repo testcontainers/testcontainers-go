3811c5bc560db3029ee938931d7e922751ce6829
Davide Salerno <dsalerno@redhat.com>
2023-06-13 15:50:25 +0200

    Added customizer to support registry with authentication
    Signed-off-by: Davide Salerno <dsalerno@redhat.com>

--- modules/dockerregistry/dockerregistry.go
+++ modules/dockerregistry/dockerregistry.go
@@ -34,3 +34,54 @@ func RunContainer(ctx context.Context, opts ...testcontainers.ContainerCustomize
 
 	return &DockerRegistryContainer{Container: container}, nil
 }
+
+// WithAuthentication customizer that will retrieve a htpasswd file into the htpasswd directory given in input
+func WithAuthentication(htpasswdPath string) testcontainers.CustomizeRequestOption {
+	return func(req *testcontainers.GenericContainerRequest) {
+		if req.Env == nil {
+			req.Env = make(map[string]string)
+		}
+
+		req.Env["REGISTRY_AUTH"] = "htpasswd"
+		req.Env["REGISTRY_AUTH_HTPASSWD_REALM"] = "Registry"
+		req.Env["REGISTRY_AUTH_HTPASSWD_PATH"] = "/auth/htpasswd"
+
+		if req.Mounts == nil {
+			req.Mounts = testcontainers.ContainerMounts{}
+		}
+
+		htpasswdMount := testcontainers.ContainerMount{
+			Source: testcontainers.GenericBindMountSource{
+				HostPath: htpasswdPath,
+			},
+			Target: "/auth",
+		}
+		req.Mounts = append(req.Mounts, htpasswdMount)
+	}
+
+}
+
+// WithData customizer that will retrieve a data directory and mount it inside the registry container
+func WithData(dataDir string) testcontainers.CustomizeRequestOption {
+	return func(req *testcontainers.GenericContainerRequest) {
+		if req.Env == nil {
+			req.Env = make(map[string]string)
+		}
+
+		req.Env["REGISTRY_STORAGE_FILESYSTEM_ROOTDIRECTORY"] = "/data"
+
+		if req.Mounts == nil {
+			req.Mounts = testcontainers.ContainerMounts{}
+		}
+
+		dataMount := testcontainers.ContainerMount{
+			Source: testcontainers.GenericBindMountSource{
+				HostPath: dataDir,
+			},
+			Target: "/data",
+		}
+
+		req.Mounts = append(req.Mounts, dataMount)
+
+	}
+}
--- modules/dockerregistry/dockerregistry_test.go
+++ modules/dockerregistry/dockerregistry_test.go
@@ -3,8 +3,15 @@ package dockerregistry
 import (
 	"context"
 	"net/http"
+	"os"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
 )
 
 func TestDockerRegistry(t *testing.T) {
@@ -41,4 +48,50 @@ func TestDockerRegistry(t *testing.T) {
 		t.Fatal(err)
 	}
 	defer resp.Body.Close()
+}
+
+func TestDockerRegistryWithAuth(t *testing.T) {
+	ctx := context.Background()
+	wd, err := os.Getwd()
+	assert.NoError(t, err)
+	container, err := RunContainer(ctx, WithAuthentication(wd+"/../../testdata/auth"), WithData(wd+"/../../testdata/data"))
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Clean up the container after the test is complete
+	t.Cleanup(func() {
+		if err := container.Terminate(ctx); err != nil {
+			t.Fatalf("failed to terminate container: %s", err)
+		}
+	})
+
+	time.Sleep(1 * time.Second)
+
+	ctx = context.Background()
+	req := testcontainers.ContainerRequest{
+		Image:           "localhost:5000/redis:5.0-alpine",
+		AlwaysPullImage: true, // make sure the authentication takes place
+		ExposedPorts:    []string{"6379/tcp"},
+		WaitingFor:      wait.ForLog("Ready to accept connections"),
+	}
+
+	redisContainer, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+	require.Nil(t, err)
+	terminateContainerOnEnd(t, ctx, redisContainer)
+}
+
+func terminateContainerOnEnd(tb testing.TB, ctx context.Context, ctr testcontainers.Container) {
+	tb.Helper()
+	if ctr == nil {
+		return
+	}
+	tb.Cleanup(func() {
+		tb.Log("terminating container")
+		require.NoError(tb, ctr.Terminate(ctx))
+	})
 }
