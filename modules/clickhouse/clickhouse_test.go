c38f7477dbe75c6ce3b01b4b6132f823eb0ff11a
Anıl Şenay <anil.senay@cimri.com>
2023-07-30 01:57:39 +0300

    feat: add clickhouse module

---
+++ modules/clickhouse/clickhouse_test.go
@@ -0,0 +1,295 @@
+package clickhouse
+
+import (
+	"context"
+	"fmt"
+	"path/filepath"
+	"testing"
+
+	ch "github.com/ClickHouse/clickhouse-go/v2"
+	"github.com/stretchr/testify/assert"
+)
+
+const dbname = "testdb"
+const user = "clickhouse"
+const password = "password"
+
+func TestClickHouse(t *testing.T) {
+	t.Run("TestClickHouse_DefaultConfig", func(t *testing.T) {
+		ctx := context.Background()
+
+		container, err := RunContainer(ctx, WithDatabase(""), WithUsername(""), WithPassword(""))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// Clean up the container after the test is complete
+		t.Cleanup(func() {
+			if err := container.Terminate(ctx); err != nil {
+				t.Fatalf("failed to terminate container: %s", err)
+			}
+		})
+
+		connectionString, err := container.ConnectionHost(ctx)
+		assert.NoError(t, err)
+
+		conn, err := ch.Open(&ch.Options{
+			Addr: []string{connectionString},
+			Auth: ch.Auth{
+				Database: container.dbName,
+				Username: container.user,
+				Password: container.password,
+			},
+		})
+		assert.NoError(t, err)
+		assert.NotNil(t, conn)
+		defer conn.Close()
+
+		err = conn.Ping(context.Background())
+		assert.NoError(t, err)
+	})
+
+	t.Run("TestClickHouse_IpPort", func(t *testing.T) {
+		ctx := context.Background()
+
+		// customInitialization {
+		container, err := RunContainer(ctx,
+			WithUsername(user),
+			WithPassword(password),
+			WithDatabase(dbname),
+		)
+		if err != nil {
+			t.Fatal(err)
+		}
+		// }
+
+		// Clean up the container after the test is complete
+		t.Cleanup(func() {
+			if err := container.Terminate(ctx); err != nil {
+				t.Fatalf("failed to terminate container: %s", err)
+			}
+		})
+
+		// connectionHost {
+		connectionString, err := container.ConnectionHost(ctx)
+		assert.NoError(t, err)
+		// }
+
+		conn, err := ch.Open(&ch.Options{
+			Addr: []string{connectionString},
+			Auth: ch.Auth{
+				Database: dbname,
+				Username: user,
+				Password: password,
+			},
+		})
+		assert.NoError(t, err)
+		assert.NotNil(t, conn)
+		defer conn.Close()
+
+		// perform assertions
+		err = conn.Exec(context.Background(), "create table if not exists test_table (id UInt64) engine = MergeTree PRIMARY KEY (id) ORDER BY (id) SETTINGS index_granularity = 8192;")
+		assert.NoError(t, err)
+
+		err = conn.Exec(context.Background(), "INSERT INTO test_table (id) VALUES (1);")
+		assert.NoError(t, err)
+
+		rows, err := conn.Query(context.Background(), "SELECT * FROM test_table")
+		assert.NoError(t, err)
+		assert.NotNil(t, rows)
+
+		type Test struct {
+			Id uint64
+		}
+
+		var data []Test
+		for rows.Next() {
+			var r Test
+			err := rows.Scan(&r.Id)
+
+			assert.NoError(t, err)
+
+			data = append(data, r)
+		}
+
+		assert.Len(t, data, 1)
+	})
+
+	t.Run("TestClickHouse_DSN", func(t *testing.T) {
+		ctx := context.Background()
+
+		container, err := RunContainer(ctx, WithUsername(user), WithPassword(password), WithDatabase(dbname))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// Clean up the container after the test is complete
+		t.Cleanup(func() {
+			if err := container.Terminate(ctx); err != nil {
+				t.Fatalf("failed to terminate container: %s", err)
+			}
+		})
+
+		// connectionString {
+		connectionString, err := container.ConnectionString(ctx, "debug=true")
+		assert.NoError(t, err)
+		// }
+
+		opts, err := ch.ParseDSN(connectionString)
+		assert.NoError(t, err)
+
+		conn, err := ch.Open(opts)
+		assert.NoError(t, err)
+		assert.NotNil(t, conn)
+		defer conn.Close()
+
+		// perform assertions
+		err = conn.Exec(context.Background(), "create table if not exists test_table (id UInt64) engine = MergeTree PRIMARY KEY (id) ORDER BY (id) SETTINGS index_granularity = 8192;")
+		assert.NoError(t, err)
+
+		err = conn.Exec(context.Background(), "INSERT INTO test_table (id) VALUES (1);")
+		assert.NoError(t, err)
+
+		rows, err := conn.Query(context.Background(), "SELECT * FROM test_table")
+		assert.NoError(t, err)
+		assert.NotNil(t, rows)
+
+		type Test struct {
+			Id uint64
+		}
+
+		var data []Test
+		for rows.Next() {
+			var r Test
+			err := rows.Scan(&r.Id)
+
+			assert.NoError(t, err)
+
+			data = append(data, r)
+		}
+
+		assert.Len(t, data, 1)
+	})
+}
+
+func TestClickHouse_WithInitScripts(t *testing.T) {
+	ctx := context.Background()
+
+	// withInitScripts {
+	container, err := RunContainer(ctx,
+		WithUsername(user),
+		WithPassword(password),
+		WithDatabase(dbname),
+		WithInitScripts(filepath.Join("testdata", "init-db.sh")),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// }
+
+	// Clean up the container after the test is complete
+	t.Cleanup(func() {
+		if err := container.Terminate(ctx); err != nil {
+			t.Fatalf("failed to terminate container: %s", err)
+		}
+	})
+
+	connectionString, err := container.ConnectionHost(ctx)
+	assert.NoError(t, err)
+
+	conn, err := ch.Open(&ch.Options{
+		Addr: []string{connectionString},
+		Auth: ch.Auth{
+			Database: dbname,
+			Username: user,
+			Password: password,
+		},
+	})
+	assert.NoError(t, err)
+	assert.NotNil(t, conn)
+	defer conn.Close()
+
+	// perform assertions
+	rows, err := conn.Query(context.Background(), "SELECT * FROM test_table")
+	assert.NoError(t, err)
+	assert.NotNil(t, rows)
+
+	type Test struct {
+		Id uint64
+	}
+
+	var data []Test
+	for rows.Next() {
+		var r Test
+		err := rows.Scan(&r.Id)
+
+		assert.NoError(t, err)
+
+		data = append(data, r)
+	}
+
+	assert.Len(t, data, 1)
+}
+
+func TestClickHouse_WithConfigFile(t *testing.T) {
+	ctx := context.Background()
+
+	container, err := RunContainer(ctx,
+		WithUsername(user),
+		WithPassword(""),
+		WithDatabase(dbname),
+		WithConfigFile(filepath.Join("testdata", "config.xml")), // allow_no_password = 1
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Clean up the container after the test is complete
+	t.Cleanup(func() {
+		if err := container.Terminate(ctx); err != nil {
+			t.Fatalf("failed to terminate container: %s", err)
+		}
+	})
+
+	connectionString, err := container.ConnectionHost(ctx)
+	assert.NoError(t, err)
+
+	conn, err := ch.Open(&ch.Options{
+		Addr: []string{connectionString},
+		Auth: ch.Auth{
+			Database: dbname,
+			Username: user,
+			// Password: password,
+		},
+	})
+	assert.NoError(t, err)
+	assert.NotNil(t, conn)
+	defer conn.Close()
+
+	// perform assertions
+	err = conn.Exec(context.Background(), "create table if not exists test_table (id UInt64) engine = MergeTree PRIMARY KEY (id) ORDER BY (id) SETTINGS index_granularity = 8192;")
+	assert.NoError(t, err)
+
+	err = conn.Exec(context.Background(), "INSERT INTO test_table (id) VALUES (1);")
+	assert.NoError(t, err)
+
+	rows, err := conn.Query(context.Background(), "SELECT * FROM test_table")
+	assert.NoError(t, err)
+	assert.NotNil(t, rows)
+
+	type Test struct {
+		Id uint64
+	}
+
+	var data []Test
+	for rows.Next() {
+		var r Test
+		err := rows.Scan(&r.Id)
+
+		assert.NoError(t, err)
+
+		data = append(data, r)
+	}
+
+	assert.Len(t, data, 1)
+}
---
+++ modules/clickhouse/go.mod
@@ -0,0 +1,58 @@
+module github.com/testcontainers/testcontainers-go/modules/clickhouse
+
+go 1.19
+
+require (
+	github.com/ClickHouse/clickhouse-go/v2 v2.12.0
+	github.com/docker/go-connections v0.4.0
+	github.com/stretchr/testify v1.8.4
+	github.com/testcontainers/testcontainers-go v0.21.0
+)
+
+require (
+	github.com/Azure/go-ansiterm v0.0.0-20210617225240-d185dfc1b5a1 // indirect
+	github.com/ClickHouse/ch-go v0.52.1 // indirect
+	github.com/Microsoft/go-winio v0.5.2 // indirect
+	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/cenkalti/backoff/v4 v4.2.0 // indirect
+	github.com/containerd/containerd v1.6.19 // indirect
+	github.com/cpuguy83/dockercfg v0.3.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/docker/distribution v2.8.2+incompatible // indirect
+	github.com/docker/docker v23.0.5+incompatible // indirect
+	github.com/docker/go-units v0.5.0 // indirect
+	github.com/go-faster/city v1.0.1 // indirect
+	github.com/go-faster/errors v0.6.1 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/imdario/mergo v0.3.15 // indirect
+	github.com/klauspost/compress v1.15.15 // indirect
+	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/moby/patternmatcher v0.5.0 // indirect
+	github.com/moby/sys/sequential v0.5.0 // indirect
+	github.com/moby/term v0.5.0 // indirect
+	github.com/morikuni/aec v1.0.0 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.1.0-rc2 // indirect
+	github.com/opencontainers/runc v1.1.5 // indirect
+	github.com/paulmach/orb v0.9.0 // indirect
+	github.com/pierrec/lz4/v4 v4.1.17 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/segmentio/asm v1.2.0 // indirect
+	github.com/shopspring/decimal v1.3.1 // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
+	go.opentelemetry.io/otel v1.16.0 // indirect
+	go.opentelemetry.io/otel/trace v1.16.0 // indirect
+	golang.org/x/exp v0.0.0-20230510235704-dd950f8aeaea // indirect
+	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	google.golang.org/genproto v0.0.0-20220617124728-180714bec0ad // indirect
+	google.golang.org/grpc v1.47.0 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gotest.tools/v3 v3.4.0 // indirect
+)
+
+replace github.com/testcontainers/testcontainers-go => ../..
