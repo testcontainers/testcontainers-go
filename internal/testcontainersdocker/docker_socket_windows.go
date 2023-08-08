dbbde5b84683682645da2bbb1bf9a065b9a9f22d
Manuel de la Peña <mdelapenya@gmail.com>
2023-08-08 18:24:46 +0200

    chore: bring back initial approach

--- internal/testcontainersdocker/docker_socket_windows.go
+++
@@ -1,7 +0,0 @@
-package testcontainersdocker
-
-// DockerSocketSchema is the Docker socket schema on Windows
-var DockerSocketSchema = "npipe://"
-
-// DockerSocketPath The socket path for Windows contains two slashes, exactly the same as the Docker Desktop socket path.
-var DockerSocketPath = "//./pipe/docker_engine"
