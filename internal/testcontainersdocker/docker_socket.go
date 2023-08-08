777cff355a80a1b2e529cc69e6f0709a34910bc2
Manuel de la Peña <mdelapenya@gmail.com>
2023-08-08 19:02:23 +0200

    fix: include schema

--- internal/testcontainersdocker/docker_socket.go
+++ internal/testcontainersdocker/docker_socket.go
@@ -45,7 +45,9 @@ func init() {
 	}
 
 	if isWindows() {
+		DockerSocketPathWithSchema = DockerSocketSchema + DockerSocketPath
+		// asign the socket path for windows right after including the schema, to avoid
+		// removing slashes from the path
 		DockerSocketPath = "//var/run/docker.sock"
-		DockerSocketPathWithSchema = DockerSocketPath
 	}
 }
