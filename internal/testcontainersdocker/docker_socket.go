c96a6f01d152cd64c8406fe5449db0059ecc039b
Manuel de la Peña <mdelapenya@gmail.com>
2023-08-08 16:54:17 +0200

    fix: compile

--- internal/testcontainersdocker/docker_socket.go
+++ internal/testcontainersdocker/docker_socket.go
@@ -1,13 +1,4 @@
 package testcontainersdocker
 
-// DockerSocketMountPath is the Docker socket mount path.
-var DockerSocketMountPath = "/var/run/docker.sock"
-
-// DockerSocketPath is the path to the Docker socket.
-var DockerSocketPath = "/var/run/docker.sock"
-
-// DockerSocketSchema is the Docker socket schema.
-var DockerSocketSchema = "unix://"
-
 // TCPSchema is the tcp schema.
 var TCPSchema = "tcp://"
--- internal/testcontainersdocker/docker_socket_unix.go
+++ internal/testcontainersdocker/docker_socket_unix.go
@@ -3,5 +3,14 @@
 
 package testcontainersdocker
 
+// DockerSocketMountPath is the Docker socket mount path.
+var DockerSocketMountPath = "/var/run/docker.sock"
+
+// DockerSocketPath is the path to the Docker socket.
+var DockerSocketPath = "/var/run/docker.sock"
+
+// DockerSocketSchema is the Docker socket schema.
+var DockerSocketSchema = "unix://"
+
 // DockerSocketPathWithSchema is the path to the Docker socket with the schema.
 var DockerSocketPathWithSchema = DockerSocketSchema + DockerSocketPath
