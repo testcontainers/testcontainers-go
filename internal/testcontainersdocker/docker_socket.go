077cbd8aabdc878d3e451d5263f3e4c9f535a730
Manuel de la Peña <mdelapenya@gmail.com>
2023-08-08 14:22:15 +0200

    chore: initialise Docker socket path on Windows properly

--- internal/testcontainersdocker/docker_socket.go
+++ internal/testcontainersdocker/docker_socket.go
@@ -20,6 +20,10 @@ var DockerSocketPathWithSchema = DockerSocketSchema + DockerSocketPath
 var TCPSchema = "tcp://"
 
 func init() {
+	initSocketPaths()
+}
+
+func initSocketPaths() {
 	const DefaultDockerHost = client.DefaultDockerHost
 
 	u, err := url.Parse(DefaultDockerHost)
@@ -29,18 +33,33 @@ func init() {
 		return
 	}
 
+	var schema string
+	var socketPath string
+
 	switch u.Scheme {
 	case "unix", "npipe":
-		DockerSocketSchema = u.Scheme + "://"
-		DockerSocketPath = u.Path
-		if !strings.HasPrefix(DockerSocketPath, "/") {
+		schema = u.Scheme + "://"
+		socketPath = u.Path
+		if !strings.HasPrefix(socketPath, "/") {
 			// seeing as the code in this module depends on DockerSocketPath having
 			// a slash (`/`) prefix, we add it here if it is missing.
 			// for the known environments, we do not foresee how the socket-path
 			// should miss the slash, however this extra if-condition is worth to
 			// save future pain from innocent users.
-			DockerSocketPath = "/" + DockerSocketPath
+			socketPath = "/" + socketPath
 		}
-		DockerSocketPathWithSchema = DockerSocketSchema + DockerSocketPath
+
+		if u.Scheme == "npipe" {
+			// the docker client package uses the npipe schema for windows
+			// docker sockets, so we need to replace it with the unix schema
+			schema = DockerSocketSchema
+			// the docker socket path on Windows using Linux containers
+			// prepends the slash to the unix socket path
+			socketPath = "/" + DockerSocketPath
+		}
 	}
+
+	DockerSocketSchema = schema
+	DockerSocketPath = socketPath
+	DockerSocketPathWithSchema = schema + socketPath
 }
---
+++ internal/testcontainersdocker/docker_socket_test.go
@@ -0,0 +1,34 @@
+package testcontainersdocker
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInitSocketPaths(t *testing.T) {
+	t.Run("Linux", func(t *testing.T) {
+		if os.Getenv("GOOS") == "windows" {
+			t.Skip("Skipping test on Windows systems")
+		}
+
+		initSocketPaths()
+
+		assert.Equal(t, "unix://", DockerSocketSchema)
+		assert.Equal(t, "/var/run/docker.sock", DockerSocketPath)
+	})
+
+	t.Run("Windows", func(t *testing.T) {
+		// Becuase the init function uses the Docker client to extract the Docker socket,
+		// and the client is already compiled for Windows, we can't test this on a non-Windows environment
+		if os.Getenv("GOOS") != "windows" {
+			t.Skip("Skipping test on non-Windows systems")
+		}
+
+		initSocketPaths()
+
+		assert.Equal(t, "unix://", DockerSocketSchema)
+		assert.Equal(t, "//var/run/docker.sock", DockerSocketPath, "The Docker socket path on Windows should have a slash prefix")
+	})
+}
