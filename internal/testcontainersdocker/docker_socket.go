881e37f870baeb0f082b13d4d0fcbbaa8bcf3cbc
Manuel de la Peña <mdelapenya@gmail.com>
2023-08-08 16:51:40 +0200

    fix: define docker socket mount path

--- internal/testcontainersdocker/docker_socket.go
+++ internal/testcontainersdocker/docker_socket.go
@@ -1,5 +1,11 @@
 package testcontainersdocker
 
+// DockerSocketMountPath is the Docker socket mount path.
+var DockerSocketMountPath = "/var/run/docker.sock"
+
+// DockerSocketPath is the path to the Docker socket.
+var DockerSocketPath = "/var/run/docker.sock"
+
 // DockerSocketSchema is the Docker socket schema.
 var DockerSocketSchema = "unix://"
 
--- internal/testcontainersdocker/docker_socket_unix.go
+++ internal/testcontainersdocker/docker_socket_unix.go
@@ -3,13 +3,5 @@
 
 package testcontainersdocker
 
-// Initialise the Docker socket paths with the Unix socket path
-// The value of these variables will be overriden by those obtained
-// from the Docker client.
-var (
-	// DockerSocketPath is the path to the Docker socket.
-	DockerSocketPath = "/var/run/docker.sock"
-
-	// DockerSocketPathWithSchema is the path to the Docker socket with the schema.
-	DockerSocketPathWithSchema = DockerSocketSchema + DockerSocketPath
-)
+// DockerSocketPathWithSchema is the path to the Docker socket with the schema.
+var DockerSocketPathWithSchema = DockerSocketSchema + DockerSocketPath
