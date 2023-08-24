79f29097e3baa6ac2e1cafbe184b1f308a87fbdb
Manuel de la Peña <mdelapenya@gmail.com>
2023-08-24 12:12:30 +0200

    fix: properly replace pymdownx.emoji in markdown

--- modulegen/internal/mkdocs/writer.go
+++ modulegen/internal/mkdocs/writer.go
@@ -2,6 +2,7 @@ package mkdocs
 
 import (
 	"os"
+	"strings"
 
 	"gopkg.in/yaml.v3"
 )
@@ -11,5 +12,14 @@ func writeConfig(configFile string, config *Config) error {
 	if err != nil {
 		return err
 	}
+
+	// simple solution to replace the empty strings, as mapping those fields
+	// into the MkDocs config is not supported yet
+	content := string(data)
+	content = strings.ReplaceAll(content, `emoji_generator: ""`, "emoji_generator: !!python/name:materialx.emoji.to_svg")
+	content = strings.ReplaceAll(content, `emoji_index: ""`, "emoji_index: !!python/name:materialx.emoji.twemoji")
+
+	data = []byte(content)
+
 	return os.WriteFile(configFile, data, 0o777)
 }
