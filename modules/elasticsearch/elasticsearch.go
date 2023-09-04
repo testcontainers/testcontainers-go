4f0909be43cfafb29af75d7551089769998642aa
Manuel de la Peña <mdelapenya@gmail.com>
2023-09-04 11:29:12 +0200

    chore: define ES contants

--- modules/elasticsearch/elasticsearch.go
+++ modules/elasticsearch/elasticsearch.go
@@ -2,12 +2,20 @@ package elasticsearch
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/testcontainers/testcontainers-go"
 )
 
 const (
-	minimalImageVersion = "8.0.0"
+	defaultHTTPPort     = "9200"
+	defaultTCPPort      = "9300"
+	defaultPassword     = "changeme"
+	minimalImageVersion = "7.9.2"
+)
+const (
+	DefaultBaseImage    = "docker.elastic.co/elasticsearch/elasticsearch"
+	DefaultBaseImageOSS = "docker.elastic.co/elasticsearch/elasticsearch-oss"
 )
 
 // ElasticsearchContainer represents the Elasticsearch container type used in the module
@@ -18,7 +26,7 @@ type ElasticsearchContainer struct {
 // RunContainer creates an instance of the Elasticsearch container type
 func RunContainer(ctx context.Context, opts ...testcontainers.ContainerCustomizer) (*ElasticsearchContainer, error) {
 	req := testcontainers.ContainerRequest{
-		Image: minimalImageVersion,
+		Image: fmt.Sprintf("%s:%s", DefaultBaseImage, minimalImageVersion),
 	}
 
 	genericContainerReq := testcontainers.GenericContainerRequest{
