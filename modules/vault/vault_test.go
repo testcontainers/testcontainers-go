0f2d1f38ceb65c90311d065b987d4d579d82db79
Matthieu MOREL <matthieu.morel35@gmail.com>
2023-09-11 10:19:37 +0200

    chore(deps): bump github.com/hashicorp/vault-client-go in /modules/vault (#1566)
    * chore(deps): bump github.com/hashicorp/vault-client-go in /modules/vault
    
    Bumps [github.com/hashicorp/vault-client-go](https://github.com/hashicorp/vault-client-go) from 0.2.0 to 0.3.3.
    - [Release notes](https://github.com/hashicorp/vault-client-go/releases)
    - [Changelog](https://github.com/hashicorp/vault-client-go/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/hashicorp/vault-client-go/compare/v0.2.0...v0.3.3)
    
    ---
    updated-dependencies:
    - dependency-name: github.com/hashicorp/vault-client-go
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * up
    
    * Update vault_test.go
    
    * Update vault_test.go
    
    * Update vault_test.go
    
    * Update vault_test.go
    
    * Update vault_test.go
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Manuel de la Peña <mdelapenya@gmail.com>

--- modules/vault/vault_test.go
+++ modules/vault/vault_test.go
@@ -100,9 +100,9 @@ func TestVaultGetSecretPathWithClient(t *testing.T) {
 	err = client.SetToken(token)
 	assert.Nil(t, err)
 
-	s, err := client.Secrets.KVv2Read(ctx, "test1")
+	s, err := client.Secrets.KvV2Read(ctx, "test1", vaultClient.WithMountPath("secret"))
 	assert.Nil(t, err)
-	assert.Equal(t, "bar1", s.Data["data"].(map[string]interface{})["foo1"])
+	assert.Equal(t, "bar1", s.Data.Data["foo1"])
 }
 
 func TestVaultWriteSecretWithClient(t *testing.T) {
@@ -116,14 +116,15 @@ func TestVaultWriteSecretWithClient(t *testing.T) {
 	err = client.SetToken(token)
 	assert.Nil(t, err)
 
-	_, err = client.Secrets.KVv2Write(ctx, "test3", schema.KVv2WriteRequest{
+	_, err = client.Secrets.KvV2Write(ctx, "test3", schema.KvV2WriteRequest{
 		Data: map[string]any{
 			"foo": "bar",
 		},
-	})
+	},
+		vaultClient.WithMountPath("secret"))
 	assert.Nil(t, err)
 
-	s, err := client.Secrets.KVv2Read(ctx, "test3")
+	s, err := client.Secrets.KvV2Read(ctx, "test3", vaultClient.WithMountPath("secret"))
 	assert.Nil(t, err)
-	assert.Equal(t, "bar", s.Data["data"].(map[string]interface{})["foo"])
+	assert.Equal(t, "bar", s.Data.Data["foo"])
 }
