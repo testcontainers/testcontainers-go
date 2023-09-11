25adc7a56af4340e215eabba4ab632a163afae53
Manuel de la Peña <mdelapenya@gmail.com>
2023-09-11 15:55:17 +0200

    Revert "chore: better container logs on errors during startup"
    This reverts commit 0e2888129309363d6e3c7dd227414b1c2c693f5a.

--- lifecycle.go
+++ lifecycle.go
@@ -2,7 +2,6 @@ package testcontainers
 
 import (
 	"context"
-	"fmt"
 	"io"
 	"strings"
 
@@ -132,7 +131,8 @@ func (c *DockerContainer) startingHook(ctx context.Context) error {
 	for _, lifecycleHooks := range c.lifecycleHooks {
 		err := containerHookFn(ctx, lifecycleHooks.PreStarts)(c)
 		if err != nil {
-			return printLogs(ctx, c, err)
+			c.printLogs(ctx)
+			return err
 		}
 	}
 
@@ -144,27 +144,30 @@ func (c *DockerContainer) startedHook(ctx context.Context) error {
 	for _, lifecycleHooks := range c.lifecycleHooks {
 		err := containerHookFn(ctx, lifecycleHooks.PostStarts)(c)
 		if err != nil {
-			return printLogs(ctx, c, err)
+			c.printLogs(ctx)
+			return err
 		}
 	}
 
 	return nil
 }
 
-// printLogs is a helper function that will print the logs of a Docker container after an error occurs
+// printLogs is a helper function that will print the logs of a Docker container
 // We are going to use this helper function to inform the user of the logs when an error occurs
-func printLogs(ctx context.Context, c *DockerContainer, rootCause error) error {
+func (c *DockerContainer) printLogs(ctx context.Context) {
 	reader, err := c.Logs(ctx)
 	if err != nil {
-		return fmt.Errorf("failed accessing container logs: %w", err)
+		c.logger.Printf("failed accessing container logs: %w\n", err)
+		return
 	}
 
 	b, err := io.ReadAll(reader)
 	if err != nil {
-		return fmt.Errorf("failed reading container logs: %w", err)
+		c.logger.Printf("failed reading container logs: %w\n", err)
+		return
 	}
 
-	return fmt.Errorf("%s: %w", b, rootCause)
+	c.logger.Printf("container logs:\n%s", b)
 }
 
 // stoppingHook is a hook that will be called before a container is stopped
