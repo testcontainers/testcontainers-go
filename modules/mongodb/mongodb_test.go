04886f112f08c2838b9cb353a82470d0bd05f953
Gorkem Karaduman <gorkemkrdmn@gmail.com>
2023-11-10 18:01:54 +0000

    Add username and password functions to mongodb

--- modules/mongodb/mongodb_test.go
+++ modules/mongodb/mongodb_test.go
@@ -3,12 +3,14 @@ package mongodb_test
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/modules/mongodb"
+	"github.com/testcontainers/testcontainers-go/wait"
 )
 
 func ExampleRunContainer() {
@@ -75,4 +77,44 @@ func ExampleRunContainer_connect() {
 
 	// Output:
 	// test
+}
+
+func ExampleRunContainer_withCredentials() {
+	ctx := context.Background()
+
+	container, err := mongodb.RunContainer(ctx,
+		testcontainers.WithImage("mongo:6"),
+		mongodb.WithUsername("root"),
+		mongodb.WithPassword("password"),
+		testcontainers.WithWaitStrategy(wait.ForLog("Waiting for connections")),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	// Clean up the container
+	defer func() {
+		if err := container.Terminate(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
+	connStr, err := container.ConnectionString(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	mongoClient, err := mongo.Connect(ctx, options.Client().ApplyURI(connStr))
+	if err != nil {
+		panic(err)
+	}
+
+	err = mongoClient.Ping(ctx, nil)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(strings.Split(connStr, "@")[0])
+
+	// Output:
+	// mongodb://root:password
 }
