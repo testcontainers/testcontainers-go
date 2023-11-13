a941bb212ddb46fe62aa127a1a46f6338c6df284
rafiramadhana <rf.ramadhana@gmail.com>
2023-11-13 19:58:33 +0700

    Run linter

--- modules/clickhouse/clickhouse_test.go
+++ modules/clickhouse/clickhouse_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/ClickHouse/clickhouse-go/v2/lib/driver"
 	"github.com/cenkalti/backoff/v4"
 	"github.com/stretchr/testify/assert"
+
 	"github.com/testcontainers/testcontainers-go"
 )
 
