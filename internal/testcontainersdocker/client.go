bcd93745437a6d9958b5fb48ba52eafcb68d15df
Manuel de la Peña <mdelapenya@gmail.com>
2023-12-19 17:20:37 +0100

    feat: send the project path to the Docker daemon as an HTTP header

--- internal/testcontainersdocker/client.go
+++ internal/testcontainersdocker/client.go
@@ -33,6 +33,7 @@ func NewClient(ctx context.Context, ops ...client.Opt) (*client.Client, error) {
 
 	opts = append(opts, client.WithHTTPHeaders(
 		map[string]string{
+			"x-tc-pp":    testcontainerssession.ProjectPath(),
 			"x-tc-sid":   testcontainerssession.SessionID(),
 			"User-Agent": "tc-go/" + internal.Version,
 		}),
