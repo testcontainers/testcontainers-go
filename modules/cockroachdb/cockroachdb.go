e81bc6a52971edd6ef133fcb3ed3c137d1d5cd6c
Rob Crowe <nobby.crowe@gmail.com>
2024-01-21 15:38:12 +0000

    start insecure crdb node

--- modules/cockroachdb/cockroachdb.go
+++ modules/cockroachdb/cockroachdb.go
@@ -2,34 +2,110 @@ package cockroachdb
 
 import (
 	"context"
+	"fmt"
+	"net"
+	"net/url"
 
 	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
+)
+
+const (
+	defaultSQLPort   = "26257"
+	defaultAdminPort = "8080"
+
+	defaultImage     = "cockroachdb/cockroach"
+	defaultImageTag  = "latest-v23.1"
+	defaultDatabase  = "defaultdb"
+	defaultStoreSize = "100%"
 )
 
 // CockroachDBContainer represents the CockroachDB container type used in the module
 type CockroachDBContainer struct {
 	testcontainers.Container
+	opts Options
+}
+
+// MustAddress panics if the address cannot be determined.
+func (c *CockroachDBContainer) MustAddress(ctx context.Context) string {
+	addr, err := c.Address(ctx)
+	if err != nil {
+		panic(err)
+	}
+	return addr
+}
+
+// Address returns the dial address to open a new connection to CockroachDB.
+func (c *CockroachDBContainer) Address(ctx context.Context) (string, error) {
+	mappedport, err := c.MappedPort(ctx, defaultSQLPort+"/tcp")
+	if err != nil {
+		return "", err
+	}
+
+	hostIP, err := c.Host(ctx)
+	if err != nil {
+		return "", err
+	}
+
+	params := url.Values{
+		"sslmode": []string{"disable"},
+	}
+
+	u := url.URL{
+		Scheme:   "postgres",
+		User:     url.User("root"),
+		Host:     net.JoinHostPort(hostIP, mappedport.Port()),
+		Path:     c.opts.Database,
+		RawQuery: params.Encode(),
+	}
+
+	return u.String(), nil
 }
 
 // RunContainer creates an instance of the CockroachDB container type
 func RunContainer(ctx context.Context, opts ...testcontainers.ContainerCustomizer) (*CockroachDBContainer, error) {
-	req := testcontainers.ContainerRequest{
-		Image: "cockroachdb/cockroach:latest-v23.1",
+	req := testcontainers.GenericContainerRequest{
+		ContainerRequest: testcontainers.ContainerRequest{
+			ExposedPorts: []string{
+				defaultSQLPort + "/tcp",
+				defaultAdminPort + "/tcp",
+			},
+			WaitingFor: wait.ForHTTP("/health").WithPort(defaultAdminPort),
+		},
+		Started: true,
 	}
 
-	genericContainerReq := testcontainers.GenericContainerRequest{
-		ContainerRequest: req,
-		Started:          true,
-	}
-
+	// apply options
+	o := defaultOptions()
 	for _, opt := range opts {
-		opt.Customize(&genericContainerReq)
+		if apply, ok := opt.(Option); ok {
+			apply(&o)
+		}
+		opt.Customize(&req)
 	}
 
-	container, err := testcontainers.GenericContainer(ctx, genericContainerReq)
+	req.Image = image(req, o)
+	req.Cmd = cmd(o)
+
+	// start
+	container, err := testcontainers.GenericContainer(ctx, req)
 	if err != nil {
 		return nil, err
 	}
+	return &CockroachDBContainer{Container: container, opts: o}, nil
+}
 
-	return &CockroachDBContainer{Container: container}, nil
+func image(req testcontainers.GenericContainerRequest, opts Options) string {
+	if req.Image != "" {
+		return req.Image
+	}
+	return fmt.Sprintf("%s:%s", defaultImage, opts.ImageTag)
+}
+
+func cmd(opts Options) []string {
+	return []string{
+		"start-single-node",
+		"--insecure",
+		"--store=type=mem,size=" + opts.StoreSize,
+	}
 }
--- modules/cockroachdb/go.mod
+++ modules/cockroachdb/go.mod
@@ -2,7 +2,10 @@ module github.com/testcontainers/testcontainers-go/modules/cockroachdb
 
 go 1.20
 
-require github.com/testcontainers/testcontainers-go v0.27.0
+require (
+	github.com/jackc/pgx/v5 v5.5.2
+	github.com/testcontainers/testcontainers-go v0.27.0
+)
 
 require (
 	dario.cat/mergo v1.0.0 // indirect
@@ -21,6 +24,8 @@ require (
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/uuid v1.5.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/klauspost/compress v1.16.0 // indirect
 	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
@@ -39,9 +44,11 @@ require (
 	github.com/tklauser/go-sysconf v0.3.12 // indirect
 	github.com/tklauser/numcpus v0.6.1 // indirect
 	github.com/yusufpapurcu/wmi v1.2.3 // indirect
+	golang.org/x/crypto v0.17.0 // indirect
 	golang.org/x/exp v0.0.0-20230510235704-dd950f8aeaea // indirect
 	golang.org/x/mod v0.11.0 // indirect
 	golang.org/x/sys v0.16.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/tools v0.10.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20230711160842-782d3b101e98 // indirect
 	google.golang.org/grpc v1.58.3 // indirect
