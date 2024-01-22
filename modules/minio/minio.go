acf1ac1f80fb4fa960fb8ac516b86bf8e4e53f35
Rahim <rahim.affulbrown@deltaflare.com>
2024-01-22 12:13:39 +0000

    get minio testing to work

--- modules/minio/minio.go
+++ modules/minio/minio.go
@@ -2,19 +2,37 @@ package minio
 
 import (
 	"context"
+	"fmt"
+	"time"
 
 	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
+)
+
+const (
+	defaultUser     = "minioadmin"
+	defaultPassword = "minioadmin"
+	defaultImage    = "minio/minio:RELEASE.2024-01-16T16-07-38Z"
 )
 
 // MinioContainer represents the Minio container type used in the module
 type MinioContainer struct {
 	testcontainers.Container
+	Username string
+	Password string
 }
 
 // RunContainer creates an instance of the Minio container type
 func RunContainer(ctx context.Context, opts ...testcontainers.ContainerCustomizer) (*MinioContainer, error) {
 	req := testcontainers.ContainerRequest{
-		Image: "minio/minio:RELEASE.2024-01-16T16-07-38Z",
+		Image:        defaultImage,
+		ExposedPorts: []string{"9000/tcp"},
+		WaitingFor:   wait.ForListeningPort("9000").WithStartupTimeout(time.Minute * 2),
+		Env: map[string]string{
+			"MINIO_ROOT_USER":     defaultUser,
+			"MINIO_ROOT_PASSWORD": defaultPassword,
+		},
+		Cmd: []string{"server", "/data"},
 	}
 
 	genericContainerReq := testcontainers.GenericContainerRequest{
@@ -26,10 +44,49 @@ func RunContainer(ctx context.Context, opts ...testcontainers.ContainerCustomize
 		opt.Customize(&genericContainerReq)
 	}
 
+	username := req.Env["MINIO_ROOT_USER"]
+	password := req.Env["MINIO_ROOT_PASSWORD"]
+	if username == "" || password == "" {
+		return nil, fmt.Errorf("username or password has not been set")
+	}
+
 	container, err := testcontainers.GenericContainer(ctx, genericContainerReq)
 	if err != nil {
 		return nil, err
 	}
 
-	return &MinioContainer{Container: container}, nil
+	return &MinioContainer{Container: container, Username: username, Password: password}, nil
+}
+
+// WithUsername
+func WithUsername(username string) testcontainers.CustomizeRequestOption {
+	return func(req *testcontainers.GenericContainerRequest) {
+		if username == "" {
+			username = defaultUser
+		}
+		req.Env["MINIO_ROOT_USER"] = username
+	}
+}
+
+// WithPassword
+func WithPassword(password string) testcontainers.CustomizeRequestOption {
+	return func(req *testcontainers.GenericContainerRequest) {
+		if password == "" {
+			password = defaultPassword
+		}
+		req.Env["MINIO_ROOT_PASSWORD"] = password
+	}
+}
+
+// ConnectionString
+func (c *MinioContainer) ConnectionString(ctx context.Context) (string, error) {
+	host, err := c.Host(ctx)
+	if err != nil {
+		return "", err
+	}
+	port, err := c.MappedPort(ctx, "9000/tcp")
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%s:%s", host, port.Port()), nil
 }
--- modules/minio/minio_test.go
+++ modules/minio/minio_test.go
@@ -2,8 +2,12 @@ package minio
 
 import (
 	"context"
+	"io"
 	"testing"
 
+	"github.com/adoublef/sdk/bytest"
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
 	"github.com/testcontainers/testcontainers-go"
 )
 
@@ -23,4 +27,49 @@ func TestMinio(t *testing.T) {
 	})
 
 	// perform assertions
+	url, err := container.ConnectionString(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	minioClient, err := minio.New(url, &minio.Options{
+		Creds:  credentials.NewStaticV4(container.Username, container.Password, ""),
+		Secure: false,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	bucketName := "testcontainers"
+	location := "eu-west-2"
+
+	// create bucket
+	err = minioClient.MakeBucket(ctx, bucketName, minio.MakeBucketOptions{Region: location})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	objectName := "testdata"
+	contentType := "applcation/octet-stream"
+
+	uploadInfo, err := minioClient.PutObject(ctx, bucketName, objectName, bytest.NewReader(bytest.MB*16), (bytest.MB * 16), minio.PutObjectOptions{ContentType: contentType})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// object is a readSeekCloser
+	object, err := minioClient.GetObject(ctx, uploadInfo.Bucket, uploadInfo.Key, minio.GetObjectOptions{})
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer object.Close()
+
+	n, err := io.Copy(io.Discard, object)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if n != bytest.MB*16 {
+		t.Fatalf("expected %d; got %d", bytest.MB*16, n)
+	}
 }
