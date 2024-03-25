32f372c44be48f9a4b271555f5e6a1e20238cbaf
Joe Cracchiolo <jjc@leapscape.com>
2024-03-25 07:29:11 -0700

    feat: add module to support InfluxDB v1.x (#1703)
    * Added module to support InfluxDB v1.x
    
    * Fix comment
    
    Co-authored-by: Eddú Meléndez Gonzales <eddu.melendez@gmail.com>
    
    * Update docs/modules/influxdb.md
    
    Fix copy/paste error
    
    Co-authored-by: Manuel de la Peña <social.mdelapenya@gmail.com>
    
    * chore: bump core to latest
    
    * chore: fix lint
    
    * chore: format
    
    * chore: use t.Cleanup
    
    * chore: use testdata
    
    * chore: add testable example for influxdb
    
    * chore: add tests for withConfig
    
    * chore: rename import
    
    * chore: change usae example
    
    * docs: include code snippet for musthaveconnectionurl
    
    * chore: rename function
    
    * chore: add to sonar
    
    * fix: proper order
    
    * chore: mod tidy
    
    * chore: always wait for the listening port
    
    * chore: remove TLS support for the moment
    
    * chore: mod tidy
    
    ---------
    
    Co-authored-by: Joseph Cracchiolo <jcracchiolo@tucows.com>
    Co-authored-by: Eddú Meléndez Gonzales <eddu.melendez@gmail.com>
    Co-authored-by: Manuel de la Peña <social.mdelapenya@gmail.com>
    Co-authored-by: Manuel de la Peña <mdelapenya@gmail.com>

---
+++ modules/influxdb/influxdb_test.go
@@ -0,0 +1,126 @@
+package influxdb_test
+
+import (
+	"context"
+	"encoding/json"
+	"path/filepath"
+	"testing"
+	"time"
+
+	influxclient "github.com/influxdata/influxdb1-client/v2"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/modules/influxdb"
+)
+
+func containerCleanup(t *testing.T, container testcontainers.Container) {
+	err := container.Terminate(context.Background())
+	require.NoError(t, err, "failed to terminate container")
+}
+
+func TestV1Container(t *testing.T) {
+	ctx := context.Background()
+	influxDbContainer, err := influxdb.RunContainer(ctx,
+		testcontainers.WithImage("influxdb:1.8.10"),
+	)
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		containerCleanup(t, influxDbContainer)
+	})
+
+	state, err := influxDbContainer.State(ctx)
+	require.NoError(t, err)
+
+	if !state.Running {
+		t.Fatal("InfluxDB container is not running")
+	}
+}
+
+func TestV2Container(t *testing.T) {
+	ctx := context.Background()
+	influxDbContainer, err := influxdb.RunContainer(ctx,
+		testcontainers.WithImage("influxdb:latest"),
+		influxdb.WithDatabase("foo"),
+		influxdb.WithUsername("root"),
+		influxdb.WithPassword("password"),
+	)
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		containerCleanup(t, influxDbContainer)
+	})
+
+	state, err := influxDbContainer.State(ctx)
+	require.NoError(t, err)
+
+	if !state.Running {
+		t.Fatal("InfluxDB container is not running")
+	}
+}
+
+func TestWithInitDb(t *testing.T) {
+	ctx := context.Background()
+	influxDbContainer, err := influxdb.RunContainer(ctx,
+		testcontainers.WithImage("influxdb:1.8.10"),
+		influxdb.WithInitDb(filepath.Join("testdata")),
+	)
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		containerCleanup(t, influxDbContainer)
+	})
+
+	if state, err := influxDbContainer.State(ctx); err != nil || !state.Running {
+		require.NoError(t, err)
+	}
+
+	cli, err := influxclient.NewHTTPClient(influxclient.HTTPConfig{
+		Addr: influxDbContainer.MustConnectionUrl(ctx),
+	})
+	require.NoError(t, err)
+	defer cli.Close()
+
+	expected_0 := `[{"statement_id":0,"Series":[{"name":"h2o_feet","tags":{"location":"coyote_creek"},"columns":["time","location","max"],"values":[[1566977040,"coyote_creek",9.964]]},{"name":"h2o_feet","tags":{"location":"santa_monica"},"columns":["time","location","max"],"values":[[1566964440,"santa_monica",7.205]]}],"Messages":null}]`
+	q := influxclient.NewQuery(`select "location", MAX("water_level") from "h2o_feet" group by "location"`, "NOAA_water_database", "s")
+	response, err := cli.Query(q)
+	require.NoError(t, err)
+
+	if response.Error() != nil {
+		t.Fatal(response.Error())
+	}
+	testJson, err := json.Marshal(response.Results)
+	require.NoError(t, err)
+
+	assert.JSONEq(t, expected_0, string(testJson))
+}
+
+func TestWithConfigFile(t *testing.T) {
+	influxVersion := "1.8.10"
+
+	influxDbContainer, err := influxdb.RunContainer(context.Background(),
+		testcontainers.WithImage("influxdb:"+influxVersion),
+		influxdb.WithConfigFile(filepath.Join("testdata", "influxdb.conf")),
+	)
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		containerCleanup(t, influxDbContainer)
+	})
+
+	if state, err := influxDbContainer.State(context.Background()); err != nil || !state.Running {
+		require.NoError(t, err)
+	}
+
+	/// influxConnectionUrl {
+	cli, err := influxclient.NewHTTPClient(influxclient.HTTPConfig{
+		Addr: influxDbContainer.MustConnectionUrl(context.Background()),
+	})
+	// }
+	require.NoError(t, err)
+	defer cli.Close()
+
+	ping, version, err := cli.Ping(5 * time.Second)
+	require.NoError(t, err)
+
+	assert.Equal(t, "1.8.10", version)
+	assert.True(t, ping > 0)
+}
