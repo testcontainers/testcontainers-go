8dbf1043bdb390900a99113874266e15ca3c0651
Laurent Saint-Félix <laurent.saintfelix@elastic.co>
2024-04-09 12:15:08 +0200

    add tests with and without ssl enabled

--- modules/elasticsearch/elasticsearch_test.go
+++ modules/elasticsearch/elasticsearch_test.go
@@ -163,6 +163,48 @@ func TestElasticsearch(t *testing.T) {
 	}
 }
 
+func TestElasticsearch8WithAndWithoutSSL(t *testing.T) {
+	t.Run("Elasticsearch 8 with SSL should provide CACert", func(t *testing.T) {
+		ctx := context.Background()
+		container, err := elasticsearch.RunContainer(ctx, testcontainers.WithImage(baseImage8))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		t.Cleanup(func() {
+			if err := container.Terminate(ctx); err != nil {
+				t.Fatalf("failed to terminate container: %s", err)
+			}
+		})
+
+		if len(container.Settings.CACert) == 0 {
+			t.Fatal("expected CA cert to not be empty")
+		}
+	})
+	t.Run("Elasticsearch 8 without SSL should not provide CACert", func(t *testing.T) {
+		ctx := context.Background()
+		container, err := elasticsearch.RunContainer(
+			ctx,
+			testcontainers.WithImage(baseImage8),
+			testcontainers.WithEnv(map[string]string{
+				"xpack.security.http.ssl.enabled": "false",
+			}))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		t.Cleanup(func() {
+			if err := container.Terminate(ctx); err != nil {
+				t.Fatalf("failed to terminate container: %s", err)
+			}
+		})
+
+		if len(container.Settings.CACert) > 0 {
+			t.Fatal("expected CA cert to be empty")
+		}
+	})
+}
+
 func TestElasticsearch8WithoutCredentials(t *testing.T) {
 	ctx := context.Background()
 
