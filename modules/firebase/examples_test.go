bf3aea5a6c7547f1a80b2c3526a32a1a907d1d51
Vytis Valentinavičius <vv@u-health.lt>
2024-05-23 13:10:26 +0300

    feat: added firebase module
    Co-authored-by: Manuel de la Peña <social.mdelapenya@gmail.com>
    Co-authored-by: Steven Hartland <stevenmhartland@gmail.com>

---
+++ modules/firebase/examples_test.go
@@ -0,0 +1,40 @@
+package firebase_test
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"path/filepath"
+
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/modules/firebase"
+)
+
+func ExampleRun() {
+	ctx := context.Background()
+
+	firebaseContainer, err := firebase.Run(ctx, "ghcr.io/u-health/docker-firebase-emulator:13.29.2",
+		firebase.WithRoot(filepath.Join("testdata", "firebase")),
+		firebase.WithCache(),
+	)
+	defer func() {
+		if err := testcontainers.TerminateContainer(firebaseContainer); err != nil {
+			log.Printf("failed to terminate container: %s", err)
+		}
+	}()
+	if err != nil {
+		log.Printf("failed to start container: %s", err)
+		return
+	}
+
+	state, err := firebaseContainer.State(ctx)
+	if err != nil {
+		log.Printf("failed to get container state: %s", err)
+		return
+	}
+
+	fmt.Println(state.Running)
+
+	// Output:
+	// true
+}
