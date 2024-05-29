3a9c72024838ec5cee3612030eaf92d210331234
Manuel de la Peña <mdelapenya@gmail.com>
2024-05-29 14:37:12 +0200

    chore: update mssql server module to the new API

--- modules/mssql/mssql.go
+++ modules/mssql/mssql.go
@@ -18,13 +18,13 @@ const (
 
 // MSSQLServerContainer represents the MSSQLServer container type used in the module
 type MSSQLServerContainer struct {
-	testcontainers.Container
+	*testcontainers.DockerContainer
 	password string
 	username string
 }
 
 func WithAcceptEULA() testcontainers.CustomizeRequestOption {
-	return func(req *testcontainers.GenericContainerRequest) error {
+	return func(req *testcontainers.Request) error {
 		req.Env["ACCEPT_EULA"] = "Y"
 
 		return nil
@@ -32,7 +32,7 @@ func WithAcceptEULA() testcontainers.CustomizeRequestOption {
 }
 
 func WithPassword(password string) testcontainers.CustomizeRequestOption {
-	return func(req *testcontainers.GenericContainerRequest) error {
+	return func(req *testcontainers.Request) error {
 		if password == "" {
 			password = defaultPassword
 		}
@@ -43,28 +43,24 @@ func WithPassword(password string) testcontainers.CustomizeRequestOption {
 }
 
 // RunContainer creates an instance of the MSSQLServer container type
-func RunContainer(ctx context.Context, opts ...testcontainers.ContainerCustomizer) (*MSSQLServerContainer, error) {
-	req := testcontainers.ContainerRequest{
+func RunContainer(ctx context.Context, opts ...testcontainers.RequestCustomizer) (*MSSQLServerContainer, error) {
+	req := testcontainers.Request{
 		Image:        defaultImage,
 		ExposedPorts: []string{defaultPort},
 		Env: map[string]string{
 			"MSSQL_SA_PASSWORD": defaultPassword,
 		},
 		WaitingFor: wait.ForLog("Recovery is complete."),
-	}
-
-	genericContainerReq := testcontainers.GenericContainerRequest{
-		ContainerRequest: req,
-		Started:          true,
+		Started:    true,
 	}
 
 	for _, opt := range opts {
-		if err := opt.Customize(&genericContainerReq); err != nil {
+		if err := opt.Customize(&req); err != nil {
 			return nil, err
 		}
 	}
 
-	container, err := testcontainers.GenericContainer(ctx, genericContainerReq)
+	container, err := testcontainers.New(ctx, req)
 	if err != nil {
 		return nil, err
 	}
@@ -72,7 +68,7 @@ func RunContainer(ctx context.Context, opts ...testcontainers.ContainerCustomize
 	username := defaultUsername
 	password := req.Env["MSSQL_SA_PASSWORD"]
 
-	return &MSSQLServerContainer{Container: container, password: password, username: username}, nil
+	return &MSSQLServerContainer{DockerContainer: container, password: password, username: username}, nil
 }
 
 func (c *MSSQLServerContainer) ConnectionString(ctx context.Context, args ...string) (string, error) {
