b008c0d8ff23f8ed70161ad498e720d92b9c9946
zhanghexian <1933690003@qq.com>
2024-05-30 16:36:07 +0800

    add vearch test package

---
+++ modules/vearch/examples_test.go
@@ -0,0 +1,38 @@
+package vearch_test
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/modules/vearch"
+)
+
+func ExampleRunContainer() {
+	// runVearchContainer {
+	ctx := context.Background()
+
+	vearchContainer, err := vearch.RunContainer(ctx, testcontainers.WithImage("vearch/vearch:v1.7.4"))
+	if err != nil {
+		log.Fatalf("failed to start container: %s", err)
+	}
+
+	// Clean up the container
+	defer func() {
+		if err := vearchContainer.Terminate(ctx); err != nil {
+			log.Fatalf("failed to terminate container: %s", err) // nolint:gocritic
+		}
+	}()
+	// }
+
+	state, err := vearchContainer.State(ctx)
+	if err != nil {
+		log.Fatalf("failed to get container state: %s", err) // nolint:gocritic
+	}
+
+	fmt.Println(state.Running)
+
+	// Output:
+	// true
+}
