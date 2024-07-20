8682198b567af594dd9ff1399915e1d08d64c3b3
Steven Hartland <stevenmhartland@gmail.com>
2024-07-20 11:53:07 +0100

    fix(inbucket): expose POP3 and wait for all ports
    Expose the POP3 port and wait for all ports which fixes the race
    condition in the tests.

--- modules/inbucket/inbucket_test.go
+++ modules/inbucket/inbucket_test.go
@@ -6,65 +6,45 @@ import (
 	"testing"
 
 	"github.com/inbucket/inbucket/pkg/rest/client"
+	"github.com/stretchr/testify/require"
 )
 
 func TestInbucket(t *testing.T) {
 	ctx := context.Background()
 
 	container, err := Run(ctx, "inbucket/inbucket:sha-2d409bb")
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
 	// Clean up the container after the test is complete
 	t.Cleanup(func() {
-		if err := container.Terminate(ctx); err != nil {
-			t.Fatalf("failed to terminate container: %s", err)
-		}
+		err := container.Terminate(ctx)
+		require.NoError(t, err)
 	})
 
 	// smtpConnection {
 	smtpUrl, err := container.SmtpConnection(ctx)
 	// }
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
 	// webInterface {
 	webInterfaceUrl, err := container.WebInterface(ctx)
 	// }
-	if err != nil {
-		t.Fatal(err)
-	}
-
+	require.NoError(t, err)
 	restClient, err := client.New(webInterfaceUrl)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
 	headers, err := restClient.ListMailbox("to@example.org")
-	if err != nil {
-		t.Fatal(err)
-	}
-	if len(headers) != 0 {
-		t.Fatalf("expected 0 messages, got %d", len(headers))
-	}
+	require.NoError(t, err)
+	require.Empty(t, headers)
 
 	msg := []byte("To: to@example.org\r\n" +
 		"Subject: Testcontainers test!\r\n" +
 		"\r\n" +
 		"This is a Testcontainers test.\r\n")
-	if err = smtp.SendMail(smtpUrl, nil, "from@example.org", []string{"to@example.org"}, msg); err != nil {
-		t.Fatal(err)
-	}
+	err = smtp.SendMail(smtpUrl, nil, "from@example.org", []string{"to@example.org"}, msg)
+	require.NoError(t, err)
 
 	headers, err = restClient.ListMailbox("to@example.org")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if len(headers) != 1 {
-		t.Fatalf("expected 1 message, got %d", len(headers))
-	}
-	// perform assertions
+	require.NoError(t, err)
+	require.Len(t, headers, 1)
 }
