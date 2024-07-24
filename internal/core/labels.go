0e34b717ae21d64f55f4ada7ae6205360ec52ea1
Steven Hartland <stevenmhartland@gmail.com>
2024-07-24 09:51:25 +0100

    fix(reaper): refactor to allow retries and fix races
    Refactor how the reaper is created to allow for proper retries of
    temporary errors such as container not found issues during startup or
    shutdown races.
    
    This eliminates the use of sync.Once which wasn't solving the problem at
    hand and replaces it with a singleton spawner with locked access.
    
    Wrap reaper errors so we can determine the cause of failures more
    easily.
    
    Fix race condition in port wait when loading from container by always
    waiting for the port first.
    
    Remove unnecessary use of buffering and invalid retry logic in reaper
    connection handling which could never recover correctly from a partial
    read.
    
    Move the reaper creation just before connections are established in
    compose to ensure its still running when the Connect calls are made.
    
    Previously the reaper was requested in NewDockerComposeWith which
    means it could have already shutdown before connections are made
    during the later sections of Up if the startup took over 1 minute.
    
    This was causing consistent failures for:
    TestDockerComposeAPIWithWaitLogStrategy
    
    Ensure that resource labels are correct so that resources aren't reaped
    when the reaper is disabled by excluding session id when reaper is
    disabled.
    
    Error when creating a reaper when the config says it's disabled so that
    we avoid hard to debug issues because a reaper is running when it
    shouldn't be.
    
    Set org.testcontainers.reap label for containers which should be reaped
    by the reaper, to prevent containers which disable the reaper from being
    incorrectly reaped.

--- internal/core/labels.go
+++ internal/core/labels.go
@@ -6,23 +6,53 @@ import (
 	"strings"
 
 	"github.com/testcontainers/testcontainers-go/internal"
+	"github.com/testcontainers/testcontainers-go/internal/config"
 )
 
 const (
-	LabelBase      = "org.testcontainers"
-	LabelLang      = LabelBase + ".lang"
-	LabelReaper    = LabelBase + ".reaper"
-	LabelRyuk      = LabelBase + ".ryuk"
+	// LabelBase is the base label for all testcontainers labels.
+	LabelBase = "org.testcontainers"
+
+	// LabelLang specifies the language which created the test container.
+	LabelLang = LabelBase + ".lang"
+
+	// LabelReaper identifies the container as a reaper.
+	LabelReaper = LabelBase + ".reaper"
+
+	// LabelRyuk identifies the container as a ryuk.
+	LabelRyuk = LabelBase + ".ryuk"
+
+	// LabelSessionID specifies the session ID of the container.
 	LabelSessionID = LabelBase + ".sessionId"
-	LabelVersion   = LabelBase + ".version"
+
+	// LabelVersion specifies the version of testcontainers which created the container.
+	LabelVersion = LabelBase + ".version"
+
+	// LabelReap specifies the container should be reaped by the reaper.
+	LabelReap = LabelBase + ".reap"
 )
 
+// DefaultLabels returns the standard set of labels which
+// includes LabelSessionID if the reaper is enabled.
 func DefaultLabels(sessionID string) map[string]string {
-	return map[string]string{
+	labels := map[string]string{
 		LabelBase:      "true",
 		LabelLang:      "go",
-		LabelSessionID: sessionID,
 		LabelVersion:   internal.Version,
+		LabelSessionID: sessionID,
+	}
+
+	if !config.Read().RyukDisabled {
+		labels[LabelReap] = "true"
+	}
+
+	return labels
+}
+
+// AddDefaultLabels adds the default labels for sessionID to target.
+func AddDefaultLabels(sessionID string, target map[string]string) {
+	for k, v := range DefaultLabels(sessionID) {
+		target[k] = v
 	}
 }
 
