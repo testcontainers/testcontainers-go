95accc4ea1b7326105a09251551d16ab7a27e081
Steven Hartland <stevenmhartland@gmail.com>
2024-08-13 17:13:07 +0100

    debugging: rate limit config dump
    Add output of the docker config if we hit rate limiting to aid in
    debugging why this is happening on github actions.
    
    Don't use logger as that's no-op when verbose is not in effect.

--- generic.go
+++ generic.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"strings"
 	"sync"
 
 	"github.com/testcontainers/testcontainers-go/internal/core"
@@ -74,6 +75,14 @@ func GenericContainer(ctx context.Context, req GenericContainerRequest) (Contain
 	}
 	if err != nil {
 		// At this point `c` might not be nil. Give the caller an opportunity to call Destroy on the container.
+		// TODO: Remove this debugging.
+		if strings.Contains(err.Error(), "toomanyrequests") {
+			// Debugging information for rate limiting.
+			cfg, err := getDockerConfig()
+			if err == nil {
+				fmt.Printf("XXX: too many requests: %+v", cfg)
+			}
+		}
 		return c, fmt.Errorf("create container: %w", err)
 	}
 
