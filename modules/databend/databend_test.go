0274491b7d0f88ac863e493f881488b596b5ff80
hantmac <hantmac@outlook.com>
2024-09-13 11:21:15 +0800

    feat: support databend module

---
+++ modules/databend/databend_test.go
@@ -0,0 +1,80 @@
+package databend_test
+
+import (
+	"context"
+	"database/sql"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/testcontainers/testcontainers-go"
+
+	"github.com/testcontainers/testcontainers-go/modules/databend"
+)
+
+func TestDatabend(t *testing.T) {
+	ctx := context.Background()
+
+	ctr, err := databend.Run(ctx, "datafuselabs/databend:v1.2.615")
+	testcontainers.CleanupContainer(t, ctr)
+	require.NoError(t, err)
+
+	// perform assertions
+	// connectionString {
+	connectionString, err := ctr.ConnectionString(ctx, "sslmode=disable")
+	// }
+	require.NoError(t, err)
+
+	mustConnectionString := ctr.MustConnectionString(ctx, "sslmode=disable")
+	require.Equal(t, connectionString, mustConnectionString)
+
+	db, err := sql.Open("databend", connectionString)
+	require.NoError(t, err)
+	defer db.Close()
+
+	err = db.Ping()
+	require.NoError(t, err)
+
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS a_table ( \n" +
+		" `col_1` VARCHAR(128) NOT NULL, \n" +
+		" `col_2` VARCHAR(128) NOT NULL, \n" +
+		")")
+	require.NoError(t, err)
+}
+
+func TestDatabendWithNonRootUserAndEmptyPassword(t *testing.T) {
+	ctx := context.Background()
+
+	ctr, err := databend.Run(ctx,
+		"datafuselabs/databend:v1.2.615",
+		databend.WithUsername("databend"),
+		databend.WithPassword("databend"))
+	testcontainers.CleanupContainer(t, ctr)
+	require.EqualError(t, err, "empty password can be used only with the root user")
+}
+
+func TestDatabendWithRootUserAndEmptyPassword(t *testing.T) {
+	ctx := context.Background()
+
+	ctr, err := databend.Run(ctx,
+		"datafuselabs/databend:v1.2.615",
+		databend.WithUsername("databend"))
+	testcontainers.CleanupContainer(t, ctr)
+	require.NoError(t, err)
+
+	// perform assertions
+	connectionString, _ := ctr.ConnectionString(ctx)
+
+	db, err := sql.Open("databend", connectionString)
+	require.NoError(t, err)
+	defer db.Close()
+
+	err = db.Ping()
+	require.NoError(t, err)
+
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS a_table ( \n" +
+		" `col_1` VARCHAR(128) NOT NULL, \n" +
+		" `col_2` VARCHAR(128) NOT NULL, \n" +
+		" PRIMARY KEY (`col_1`, `col_2`) \n" +
+		")")
+	require.NoError(t, err)
+}
---
+++ modules/databend/examples_test.go
@@ -0,0 +1,86 @@
+package databend_test
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+
+	"github.com/testcontainers/testcontainers-go"
+
+	"github.com/testcontainers/testcontainers-go/modules/databend"
+)
+
+func ExampleRun() {
+	ctx := context.Background()
+
+	databendContainer, err := databend.Run(ctx,
+		"datafuselabs/databend:v1.2.615",
+		databend.WithUsername("root"),
+		databend.WithPassword("password"),
+	)
+	defer func() {
+		if err := testcontainers.TerminateContainer(databendContainer); err != nil {
+			log.Printf("failed to terminate container: %s", err)
+		}
+	}()
+	if err != nil {
+		log.Printf("failed to start container: %s", err)
+		return
+	}
+	// }
+
+	state, err := databendContainer.State(ctx)
+	if err != nil {
+		log.Printf("failed to get container state: %s", err)
+		return
+	}
+
+	fmt.Println(state.Running)
+}
+
+func ExampleRun_connect() {
+	ctx := context.Background()
+
+	databendContainer, err := databend.Run(ctx,
+		"datafuselabs/databend:v1.2.615",
+		databend.WithUsername("root"),
+		databend.WithPassword("password"),
+	)
+	defer func() {
+		if err := testcontainers.TerminateContainer(databendContainer); err != nil {
+			log.Printf("failed to terminate container: %s", err)
+		}
+	}()
+	if err != nil {
+		log.Printf("failed to start container: %s", err)
+		return
+	}
+
+	connectionString, err := databendContainer.ConnectionString(ctx)
+	if err != nil {
+		log.Printf("failed to get connection string: %s", err)
+		return
+	}
+
+	db, err := sql.Open("databend", connectionString)
+	if err != nil {
+		log.Printf("failed to connect to Databend: %s", err)
+		return
+	}
+	defer db.Close()
+
+	if err = db.Ping(); err != nil {
+		log.Printf("failed to ping Databend: %s", err)
+		return
+	}
+	var i int
+	row, err := db.Query("select 1")
+	err = row.Scan(&i)
+	if err != nil {
+		log.Printf("failed to scan row: %s", err)
+		return
+	}
+
+	fmt.Println(i)
+}
