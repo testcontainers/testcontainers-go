b0eedbd3838b1d2c2c1a0745b9fd8ab0e8d3127e
Manuel de la Peña <mdelapenya@gmail.com>
2024-09-24 13:00:53 +0200

    chore: more readable tests

--- modules/etcd/cmd_test.go
+++ modules/etcd/cmd_test.go
@@ -7,110 +7,102 @@ import (
 )
 
 func TestConfigureCMD(t *testing.T) {
-	tests := []struct {
-		name     string
-		settings options
-		want     []string
-	}{
-		{
-			name:     "default",
-			settings: options{},
-			want:     []string{"etcd", "--name=default"},
-		},
-		{
-			name: "with-node",
-			settings: options{
-				Nodes: []string{"node1"},
-			},
-			want: []string{
-				"etcd",
-				"--name=node1",
-				"--initial-advertise-peer-urls=http://node1:2380",
-				"--advertise-client-urls=http://node1:2379",
-				"--listen-peer-urls=http://0.0.0.0:2380",
-				"--listen-client-urls=http://0.0.0.0:2379",
-				"--initial-cluster-state=new",
-				"--initial-cluster=node1=http://node1:2380",
-			},
-		},
-		{
-			name: "with-node-datadir",
-			settings: options{
-				Nodes:        []string{"node1"},
-				MountDataDir: true,
-			},
-			want: []string{
-				"etcd",
-				"--name=node1",
-				"--initial-advertise-peer-urls=http://node1:2380",
-				"--advertise-client-urls=http://node1:2379",
-				"--listen-peer-urls=http://0.0.0.0:2380",
-				"--listen-client-urls=http://0.0.0.0:2379",
-				"--initial-cluster-state=new",
-				"--initial-cluster=node1=http://node1:2380",
-				"--data-dir=/data.etcd",
-			},
-		},
-		{
-			name: "with-node-datadir-additional-args",
-			settings: options{
-				Nodes:          []string{"node1"},
-				MountDataDir:   true,
-				AdditionalArgs: []string{"--auto-compaction-retention=1"},
-			},
-			want: []string{
-				"etcd",
-				"--name=node1",
-				"--initial-advertise-peer-urls=http://node1:2380",
-				"--advertise-client-urls=http://node1:2379",
-				"--listen-peer-urls=http://0.0.0.0:2380",
-				"--listen-client-urls=http://0.0.0.0:2379",
-				"--initial-cluster-state=new",
-				"--initial-cluster=node1=http://node1:2380",
-				"--data-dir=/data.etcd",
-				"--auto-compaction-retention=1",
-			},
-		},
-		{
-			name: "with-cluster",
-			settings: options{
-				Nodes: []string{"node1", "node2"},
-			},
-			want: []string{
-				"etcd",
-				"--name=node1",
-				"--initial-advertise-peer-urls=http://node1:2380",
-				"--advertise-client-urls=http://node1:2379",
-				"--listen-peer-urls=http://0.0.0.0:2380",
-				"--listen-client-urls=http://0.0.0.0:2379",
-				"--initial-cluster-state=new",
-				"--initial-cluster=node1=http://node1:2380,node2=http://node2:2380",
-			},
-		},
-		{
-			name: "with-cluster-token",
-			settings: options{
-				Nodes:        []string{"node1", "node2"},
-				ClusterToken: "token",
-			},
-			want: []string{
-				"etcd",
-				"--name=node1",
-				"--initial-advertise-peer-urls=http://node1:2380",
-				"--advertise-client-urls=http://node1:2379",
-				"--listen-peer-urls=http://0.0.0.0:2380",
-				"--listen-client-urls=http://0.0.0.0:2379",
-				"--initial-cluster-state=new",
-				"--initial-cluster=node1=http://node1:2380,node2=http://node2:2380",
-				"--initial-cluster-token=token",
-			},
-		},
-	}
+	t.Run("default", func(t *testing.T) {
+		got := configureCMD(options{})
+		want := []string{"etcd", "--name=default"}
+		require.Equal(t, want, got)
+	})
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got := configureCMD(tt.settings)
-			require.Equal(t, tt.want, got)
+	t.Run("with-node", func(t *testing.T) {
+		got := configureCMD(options{
+			Nodes: []string{"node1"},
 		})
-	}
+		want := []string{
+			"etcd",
+			"--name=node1",
+			"--initial-advertise-peer-urls=http://node1:2380",
+			"--advertise-client-urls=http://node1:2379",
+			"--listen-peer-urls=http://0.0.0.0:2380",
+			"--listen-client-urls=http://0.0.0.0:2379",
+			"--initial-cluster-state=new",
+			"--initial-cluster=node1=http://node1:2380",
+		}
+		require.Equal(t, want, got)
+	})
+
+	t.Run("with-node-datadir", func(t *testing.T) {
+		got := configureCMD(options{
+			Nodes:        []string{"node1"},
+			MountDataDir: true,
+		})
+		want := []string{
+			"etcd",
+			"--name=node1",
+			"--initial-advertise-peer-urls=http://node1:2380",
+			"--advertise-client-urls=http://node1:2379",
+			"--listen-peer-urls=http://0.0.0.0:2380",
+			"--listen-client-urls=http://0.0.0.0:2379",
+			"--initial-cluster-state=new",
+			"--initial-cluster=node1=http://node1:2380",
+			"--data-dir=/data.etcd",
+		}
+		require.Equal(t, want, got)
+	})
+
+	t.Run("with-node-datadir-additional-args", func(t *testing.T) {
+		got := configureCMD(options{
+			Nodes:          []string{"node1"},
+			MountDataDir:   true,
+			AdditionalArgs: []string{"--auto-compaction-retention=1"},
+		})
+		want := []string{
+			"etcd",
+			"--name=node1",
+			"--initial-advertise-peer-urls=http://node1:2380",
+			"--advertise-client-urls=http://node1:2379",
+			"--listen-peer-urls=http://0.0.0.0:2380",
+			"--listen-client-urls=http://0.0.0.0:2379",
+			"--initial-cluster-state=new",
+			"--initial-cluster=node1=http://node1:2380",
+			"--data-dir=/data.etcd",
+			"--auto-compaction-retention=1",
+		}
+		require.Equal(t, want, got)
+	})
+
+	t.Run("with-cluster", func(t *testing.T) {
+		got := configureCMD(options{
+			Nodes: []string{"node1", "node2"},
+		})
+		want := []string{
+			"etcd",
+			"--name=node1",
+			"--initial-advertise-peer-urls=http://node1:2380",
+			"--advertise-client-urls=http://node1:2379",
+			"--listen-peer-urls=http://0.0.0.0:2380",
+			"--listen-client-urls=http://0.0.0.0:2379",
+			"--initial-cluster-state=new",
+			"--initial-cluster=node1=http://node1:2380,node2=http://node2:2380",
+		}
+		require.Equal(t, want, got)
+	})
+
+	t.Run("with-cluster-token", func(t *testing.T) {
+		got := configureCMD(options{
+			Nodes:        []string{"node1", "node2"},
+			ClusterToken: "token",
+		})
+		want := []string{
+			"etcd",
+			"--name=node1",
+			"--initial-advertise-peer-urls=http://node1:2380",
+			"--advertise-client-urls=http://node1:2379",
+			"--listen-peer-urls=http://0.0.0.0:2380",
+			"--listen-client-urls=http://0.0.0.0:2379",
+			"--initial-cluster-state=new",
+			"--initial-cluster=node1=http://node1:2380,node2=http://node2:2380",
+			"--initial-cluster-token=token",
+		}
+		require.Equal(t, want, got)
+	})
 }
