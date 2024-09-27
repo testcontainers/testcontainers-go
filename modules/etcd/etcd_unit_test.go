f52460d030715fd67baecee875f9bcac10141e35
Manuel de la Peña <mdelapenya@gmail.com>
2024-09-27 13:52:48 +0200

    chore: extract to function

--- modules/etcd/etcd_unit_test.go
+++ modules/etcd/etcd_unit_test.go
@@ -27,57 +27,8 @@ func TestRunCluster1Node(t *testing.T) {
 }
 
 func TestRunClusterMultipleNodes(t *testing.T) {
-	testCases := []struct {
-		name  string
-		node1 string
-		node2 string
-		nodes []string
-	}{
-		{
-			name:  "2-nodes",
-			node1: "etcd-1",
-			node2: "etcd-2",
-			nodes: []string{},
-		},
-		{
-			name:  "3-nodes",
-			node1: "etcd-1",
-			node2: "etcd-2",
-			nodes: []string{"etcd-3"},
-		},
-	}
-
-	const clusterToken string = "My-cluster-t0k3n"
-
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			ctx := context.Background()
-
-			ctr, err := Run(ctx, "gcr.io/etcd-development/etcd:v3.5.14", WithNodes(tc.node1, tc.node2, tc.nodes...), WithClusterToken(clusterToken))
-			testcontainers.CleanupContainer(t, ctr)
-			require.NoError(t, err)
-
-			require.Equal(t, clusterToken, ctr.opts.clusterToken)
-
-			// the topology has one parent node, one child node and optionally more child nodes
-			// depending on the number of nodes specified
-			require.Len(t, ctr.childNodes, 1+len(tc.nodes))
-
-			for i, node := range ctr.childNodes {
-				require.Empty(t, node.childNodes) // child nodes has no children
-
-				c, r, err := node.Exec(ctx, []string{"etcdctl", "member", "list"}, tcexec.Multiplexed())
-				require.NoError(t, err)
-
-				output, err := io.ReadAll(r)
-				require.NoError(t, err)
-				require.Contains(t, string(output), fmt.Sprintf("etcd-%d", i+1))
-
-				require.Zero(t, c)
-				require.Equal(t, clusterToken, node.opts.clusterToken)
-			}
-		})
-	}
+	t.Run("2-nodes", testCluster(t, "etcd-1", "etcd-2"))
+	t.Run("3-nodes", testCluster(t, "etcd-1", "etcd-2", "etcd-3"))
 }
 
 func TestTerminate(t *testing.T) {
@@ -103,4 +54,39 @@ func TestTerminate(t *testing.T) {
 
 	_, err = cli.NetworkInspect(context.Background(), ctr.opts.clusterNetwork.ID, network.InspectOptions{})
 	require.True(t, errdefs.IsNotFound(err))
+}
+
+// testCluster is a helper function to test the creation of an etcd cluster with the specified nodes.
+func testCluster(t *testing.T, node1 string, node2 string, nodes ...string) func(t *testing.T) {
+	t.Helper()
+
+	return func(tt *testing.T) {
+		const clusterToken string = "My-cluster-t0k3n"
+
+		ctx := context.Background()
+
+		ctr, err := Run(ctx, "gcr.io/etcd-development/etcd:v3.5.14", WithNodes(node1, node2, nodes...), WithClusterToken(clusterToken))
+		testcontainers.CleanupContainer(t, ctr)
+		require.NoError(tt, err)
+
+		require.Equal(tt, clusterToken, ctr.opts.clusterToken)
+
+		// the topology has one parent node, one child node and optionally more child nodes
+		// depending on the number of nodes specified
+		require.Len(tt, ctr.childNodes, 1+len(nodes))
+
+		for i, node := range ctr.childNodes {
+			require.Empty(t, node.childNodes) // child nodes has no children
+
+			c, r, err := node.Exec(ctx, []string{"etcdctl", "member", "list"}, tcexec.Multiplexed())
+			require.NoError(tt, err)
+
+			output, err := io.ReadAll(r)
+			require.NoError(t, err)
+			require.Contains(t, string(output), fmt.Sprintf("etcd-%d", i+1))
+
+			require.Zero(tt, c)
+			require.Equal(tt, clusterToken, node.opts.clusterToken)
+		}
+	}
 }
