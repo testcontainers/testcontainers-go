51a4c82bf0d386932c7e136e3d7aceefc464e352
Manuel de la Peña <mdelapenya@gmail.com>
2024-10-21 12:39:52 +0200

    chore: not needed t.Run

--- modules/meilisearch/meilisearch_test.go
+++ modules/meilisearch/meilisearch_test.go
@@ -43,45 +43,43 @@ func TestMeilisearch_WithDataDump(t *testing.T) {
 	testcontainers.CleanupContainer(t, ctr)
 	require.NoError(t, err)
 
-	t.Run("Connect to Address", func(t *testing.T) {
-		address, err := ctr.Address(ctx)
-		require.NoError(t, err)
+	address, err := ctr.Address(ctx)
+	require.NoError(t, err)
 
-		client := http.DefaultClient
+	client := http.DefaultClient
 
-		req, err := http.NewRequest(http.MethodGet, address, nil)
-		require.NoError(t, err)
+	req, err := http.NewRequest(http.MethodGet, address, nil)
+	require.NoError(t, err)
 
-		resp, err := client.Do(req)
-		require.NoError(t, err)
-		resp.Body.Close() // not closing the body in a defer as it's not used anymore
+	resp, err := client.Do(req)
+	require.NoError(t, err)
+	resp.Body.Close() // not closing the body in a defer as it's not used anymore
 
-		require.EqualValues(t, http.StatusOK, resp.StatusCode)
+	require.EqualValues(t, http.StatusOK, resp.StatusCode)
 
-		path, err := url.JoinPath(address, "/indexes/movies/documents/1212")
-		require.NoError(t, err)
+	path, err := url.JoinPath(address, "/indexes/movies/documents/1212")
+	require.NoError(t, err)
 
-		req, err = http.NewRequest(http.MethodGet, path, nil)
-		require.NoError(t, err)
-		req.Header.Set("Content-Type", "application/json")
-		req.Header.Set("Authorization", "Bearer my-master-key")
+	req, err = http.NewRequest(http.MethodGet, path, nil)
+	require.NoError(t, err)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer my-master-key")
 
-		resp, err = client.Do(req)
-		require.NoError(t, err)
-		defer resp.Body.Close()
+	resp, err = client.Do(req)
+	require.NoError(t, err)
+	defer resp.Body.Close()
 
-		require.EqualValues(t, http.StatusOK, resp.StatusCode)
+	require.EqualValues(t, http.StatusOK, resp.StatusCode)
 
-		bodyBytes, err := io.ReadAll(resp.Body)
-		require.NoError(t, err)
+	bodyBytes, err := io.ReadAll(resp.Body)
+	require.NoError(t, err)
 
-		// Assert the response of that document.
-		require.JSONEq(t, `{
+	// Assert the response of that document.
+	require.JSONEq(t, `{
   "movie_id": 1212,
   "overview": "When a scientists daughter is kidnapped, American Ninja, attempts to find her, but this time he teams up with a youngster he has trained in the ways of the ninja.",
   "poster": "https://image.tmdb.org/t/p/w1280/iuAQVI4mvjI83wnirpD8GVNRVuY.jpg",
   "release_date": 725846400,
   "title": "American Ninja 5"
 }`, string(bodyBytes))
-	})
 }
