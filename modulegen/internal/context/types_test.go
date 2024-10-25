8c17d3e6ca6820a6406efa9f41c629676b4580cc
Manuel de la Peña <mdelapenya@gmail.com>
2024-10-25 14:06:23 +0200

    chore: move tests

--- modulegen/internal/context/types_test.go
+++ modulegen/internal/context/types_test.go
@@ -9,6 +9,70 @@ import (
 	"github.com/testcontainers/testcontainers-go/modulegen/internal/context"
 )
 
+func TestModule(t *testing.T) {
+	tests := []struct {
+		name               string
+		module             context.TestcontainersModule
+		expectedEntrypoint string
+		expectedTitle      string
+	}{
+		{
+			name: "Module with title",
+			module: context.TestcontainersModule{
+				Name:      "mongoDB",
+				IsModule:  true,
+				Image:     "mongodb:latest",
+				TitleName: "MongoDB",
+			},
+			expectedEntrypoint: "Run",
+			expectedTitle:      "MongoDB",
+		},
+		{
+			name: "Module without title",
+			module: context.TestcontainersModule{
+				Name:     "mongoDB",
+				IsModule: true,
+				Image:    "mongodb:latest",
+			},
+			expectedEntrypoint: "Run",
+			expectedTitle:      "Mongodb",
+		},
+		{
+			name: "Example with title",
+			module: context.TestcontainersModule{
+				Name:      "mongoDB",
+				IsModule:  false,
+				Image:     "mongodb:latest",
+				TitleName: "MongoDB",
+			},
+			expectedEntrypoint: "run",
+			expectedTitle:      "MongoDB",
+		},
+		{
+			name: "Example without title",
+			module: context.TestcontainersModule{
+				Name:     "mongoDB",
+				IsModule: false,
+				Image:    "mongodb:latest",
+			},
+
+			expectedEntrypoint: "run",
+			expectedTitle:      "Mongodb",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			module := test.module
+
+			require.Equal(t, "mongodb", module.Lower())
+			require.Equal(t, test.expectedTitle, module.Title())
+			require.Equal(t, "Container", module.ContainerName())
+			require.Equal(t, test.expectedEntrypoint, module.Entrypoint())
+		})
+	}
+}
+
 func TestModule_Validate(outer *testing.T) {
 	outer.Parallel()
 
