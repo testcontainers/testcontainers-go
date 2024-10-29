c553a3d7879bdee8a6da7e43cbb40e45b13a60b0
Manuel de la Peña <mdelapenya@gmail.com>
2024-10-29 11:09:28 +0100

    chore: simplify test dirs creation

--- modulegen/context_test.go
+++ modulegen/context_test.go
@@ -17,22 +17,17 @@ func getTestRootContext(t *testing.T) context.Context {
 	return context.New(filepath.Dir(current))
 }
 
+// copy examples and modules dir to the test context
 func copyModulesAndExamples(t *testing.T, ctx context.Context) {
 	t.Helper()
 	rootCtx := getTestRootContext(t)
-
-	// copy examples and modules dir to the test context
-	err := os.MkdirAll(filepath.Join(ctx.RootDir, "examples"), 0o777)
-	require.NoError(t, err)
-	err = os.MkdirAll(filepath.Join(ctx.RootDir, "modules"), 0o777)
-	require.NoError(t, err)
 
 	examples, err := rootCtx.GetExamples()
 	require.NoError(t, err)
 
 	for _, example := range examples {
 		// create dirs for each example, as we do not need to create the rest of the files
-		err = os.MkdirAll(filepath.Join(ctx.RootDir, "examples", example), 0o777)
+		err := os.MkdirAll(filepath.Join(ctx.RootDir, "examples", example), 0o777)
 		require.NoError(t, err)
 	}
 
@@ -41,7 +36,7 @@ func copyModulesAndExamples(t *testing.T, ctx context.Context) {
 
 	for _, module := range modules {
 		// create dirs for each module, as we do not need to create the rest of the files
-		err = os.MkdirAll(filepath.Join(ctx.RootDir, "modules", module), 0o777)
+		err := os.MkdirAll(filepath.Join(ctx.RootDir, "modules", module), 0o777)
 		require.NoError(t, err)
 	}
 }
