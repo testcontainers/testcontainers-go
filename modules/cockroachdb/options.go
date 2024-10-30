9944ec20f22b4dc77cc55f4a93fd0f9eade4051b
Martin Asquino <martin.asquino@gmail.com>
2024-10-30 13:25:39 +0000

    feat: add WithStatements option and expose recommended settings as a list of statements

--- modules/cockroachdb/options.go
+++ modules/cockroachdb/options.go
@@ -3,11 +3,12 @@ package cockroachdb
 import "github.com/testcontainers/testcontainers-go"
 
 type options struct {
-	Database  string
-	User      string
-	Password  string
-	StoreSize string
-	TLS       *TLSConfig
+	Database   string
+	User       string
+	Password   string
+	StoreSize  string
+	TLS        *TLSConfig
+	Statements []string
 }
 
 func defaultOptions() options {
@@ -65,5 +66,25 @@ func WithStoreSize(size string) Option {
 func WithTLS(cfg *TLSConfig) Option {
 	return func(o *options) {
 		o.TLS = cfg
+	}
+}
+
+// ClusterDefaults are the settings recommended by Cockroach Labs for testing clusters.
+// See https://www.cockroachlabs.com/docs/stable/local-testing for more information.
+var ClusterDefaults []string = []string{
+	"SET CLUSTER SETTING kv.range_merge.queue_interval = '50ms'",
+	"SET CLUSTER SETTING jobs.registry.interval.gc = '30s'",
+	"SET CLUSTER SETTING jobs.registry.interval.cancel = '180s'",
+	"SET CLUSTER SETTING jobs.retention_time = '15s'",
+	"SET CLUSTER SETTING sql.stats.automatic_collection.enabled = false",
+	"SET CLUSTER SETTING kv.range_split.by_load_merge_delay = '5s'",
+	`ALTER RANGE default CONFIGURE ZONE USING "gc.ttlseconds" = 600`,
+	`ALTER DATABASE system CONFIGURE ZONE USING "gc.ttlseconds" = 600`,
+}
+
+// WithStatements sets the statements to run on the CockroachDB cluster once the container is ready.
+func WithStatements(statements ...string) Option {
+	return func(o *options) {
+		o.Statements = statements
 	}
 }
