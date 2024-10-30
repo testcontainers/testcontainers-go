2553165d884a92a0d4afc7283a68331b70cbe040
Martin Asquino <martin.asquino@gmail.com>
2024-10-30 15:29:02 +0000

    chore: improvement documentation and set default value for statements in defaultOptions

--- modules/cockroachdb/examples_test.go
+++ modules/cockroachdb/examples_test.go
@@ -2,6 +2,7 @@ package cockroachdb_test
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"log"
 	"net/url"
@@ -49,4 +50,62 @@ func ExampleRun() {
 	// Output:
 	// true
 	// postgres://root@localhost:xxx/defaultdb?sslmode=disable
+}
+
+func ExampleRun_withRecommendedSettings() {
+	ctx := context.Background()
+
+	cockroachdbContainer, err := cockroachdb.Run(ctx, "cockroachdb/cockroach:latest-v23.1", cockroachdb.WithStatements(cockroachdb.ClusterDefaults...))
+	defer func() {
+		if err := testcontainers.TerminateContainer(cockroachdbContainer); err != nil {
+			log.Printf("failed to terminate container: %s", err)
+		}
+	}()
+	if err != nil {
+		log.Printf("failed to start container: %s", err)
+		return
+	}
+
+	state, err := cockroachdbContainer.State(ctx)
+	if err != nil {
+		log.Printf("failed to get container state: %s", err)
+		return
+	}
+	fmt.Println(state.Running)
+
+	addr, err := cockroachdbContainer.ConnectionString(ctx)
+	if err != nil {
+		log.Printf("failed to get connection string: %s", err)
+		return
+	}
+
+	db, err := sql.Open("pgx/v5", addr)
+	if err != nil {
+		log.Printf("failed to open connection: %s", err)
+		return
+	}
+	defer func() {
+		if err := db.Close(); err != nil {
+			log.Printf("failed to close connection: %s", err)
+		}
+	}()
+
+	var queueInterval string
+	if err := db.QueryRow("SHOW CLUSTER SETTING kv.range_merge.queue_interval").Scan(&queueInterval); err != nil {
+		log.Printf("failed to scan row: %s", err)
+		return
+	}
+	fmt.Println(queueInterval)
+
+	var statsCollectionEnabled bool
+	if err := db.QueryRow("SHOW CLUSTER SETTING sql.stats.automatic_collection.enabled").Scan(&statsCollectionEnabled); err != nil {
+		log.Printf("failed to scan row: %s", err)
+		return
+	}
+	fmt.Println(statsCollectionEnabled)
+
+	// Output:
+	// true
+	// 00:00:00.05
+	// false
 }
