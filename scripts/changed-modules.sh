2e35ae8d07bf2423f5df01e356d7d21e3d60761c
Manuel de la Peña <mdelapenya@gmail.com>
2024-11-07 11:46:32 +0100

    fix: proper all_modified_files quotes

--- scripts/changed-modules.sh
+++ scripts/changed-modules.sh
@@ -2,7 +2,7 @@
 
 # How to test this script, run it with the required environment variables:
 # 1. A Go file from the core module is modified:
-#    ALL_CHANGED_FILES="examples/nginx/go.mod, examples/foo/a.txt, a/b/c/d/a.go" ALL_AVAILABLE_MODULES="$(./scripts/list-modules.sh)" ./scripts/changed-modules.sh
+#    ALL_CHANGED_FILES="examples/nginx/go.mod examples/foo/a.txt a/b/c/d/a.go" ALL_AVAILABLE_MODULES="$(./scripts/list-modules.sh)" ./scripts/changed-modules.sh
 #    The output should be: all modules.
 #
 # 2. A file from a module in the modules dir is modified:
@@ -20,6 +20,10 @@
 # 5. A non-Go file from the core dir is modified:
 #    ALL_CHANGED_FILES="docs/README.md" ALL_AVAILABLE_MODULES="$(./scripts/list-modules.sh)" ./scripts/changed-modules.sh
 #    The output should be: all modules.
+#
+# 6. A file from two modules in the modules dir are modified:
+#    ALL_CHANGED_FILES="modules/nginx/go.mod modules/localstack/go.mod" ALL_AVAILABLE_MODULES="$(./scripts/list-modules.sh)" ./scripts/changed-modules.sh
+#    The output should be: the modules/nginx and modules/localstack modules.
 #
 # There is room for improvement in this script. For example, it could detect if the changes applied to the docs or the .github dirs, and then do not include any module in the list.
 # But then we would need to verify the CI scripts to ensure that the job receives the correct modules to build.
