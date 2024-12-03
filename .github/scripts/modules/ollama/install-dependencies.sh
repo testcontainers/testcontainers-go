e4d2234df5a69bbf72e593cc095075010a8cce35
Manuel de la Peña <mdelapenya@gmail.com>
2024-12-03 22:28:33 +0100

    chore: stop ollama using systemctl

--- .github/scripts/modules/ollama/install-dependencies.sh
+++ .github/scripts/modules/ollama/install-dependencies.sh
@@ -2,5 +2,5 @@
 
 curl -fsSL https://ollama.com/install.sh | sh
 
-# kill any running ollama process so that the tests can start from
-pkill ollama
+# kill any running ollama process so that the tests can start from a clean state
+sudo systemctl stop ollama.service
