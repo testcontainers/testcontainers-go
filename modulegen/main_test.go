6c6568b39d07fcece135bf6ce81c4a280ccd2926
Matthieu MOREL <matthieu.morel35@gmail.com>
2025-01-27 17:43:33 +0100

    chore: enable context-as-argument from revive linter (#2946)
    Signed-off-by: Matthieu MOREL <matthieu.morel35@gmail.com>

--- modulegen/main_test.go
+++ modulegen/main_test.go
@@ -296,7 +296,7 @@ func TestGenerate(t *testing.T) {
 	assertModuleContent(t, module, filepath.Join(generatedTemplatesDir, moduleNameLower+".go"))
 	assertGoModContent(t, module, originalConfig.Extra.LatestVersion, filepath.Join(generatedTemplatesDir, "go.mod"))
 	assertMakefileContent(t, module, filepath.Join(generatedTemplatesDir, "Makefile"))
-	assertMkdocsNavItems(t, module, originalConfig, tmpCtx)
+	assertMkdocsNavItems(t, tmpCtx, module, originalConfig)
 }
 
 func TestGenerateModule(t *testing.T) {
@@ -352,7 +352,7 @@ func TestGenerateModule(t *testing.T) {
 	assertModuleContent(t, module, filepath.Join(generatedTemplatesDir, moduleNameLower+".go"))
 	assertGoModContent(t, module, originalConfig.Extra.LatestVersion, filepath.Join(generatedTemplatesDir, "go.mod"))
 	assertMakefileContent(t, module, filepath.Join(generatedTemplatesDir, "Makefile"))
-	assertMkdocsNavItems(t, module, originalConfig, tmpCtx)
+	assertMkdocsNavItems(t, tmpCtx, module, originalConfig)
 }
 
 // assert content module file in the docs
@@ -480,9 +480,9 @@ func assertMakefileContent(t *testing.T, module context.TestcontainersModule, ma
 }
 
 // assert content in the nav items from mkdocs.yml
-func assertMkdocsNavItems(t *testing.T, module context.TestcontainersModule, originalConfig *mkdocs.Config, tmpCtx context.Context) {
+func assertMkdocsNavItems(t *testing.T, ctx context.Context, module context.TestcontainersModule, originalConfig *mkdocs.Config) {
 	t.Helper()
-	config, err := mkdocs.ReadConfig(tmpCtx.MkdocsConfigFile())
+	config, err := mkdocs.ReadConfig(ctx.MkdocsConfigFile())
 	require.NoError(t, err)
 
 	parentDir := module.ParentDir()
