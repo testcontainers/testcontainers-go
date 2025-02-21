88eba2590d8d56b18a9e2a9b10e1e9deeecc31d5
Manuel de la Peña <mdelapenya@gmail.com>
2025-02-21 16:55:36 +0100

    chore: extract dockerignore to the image package

--- container.go
+++ container.go
@@ -19,9 +19,9 @@ import (
 	"github.com/docker/docker/pkg/archive"
 	"github.com/docker/go-connections/nat"
 	"github.com/google/uuid"
-	"github.com/moby/patternmatcher/ignorefile"
 
 	tcexec "github.com/testcontainers/testcontainers-go/exec"
+	"github.com/testcontainers/testcontainers-go/image"
 	"github.com/testcontainers/testcontainers-go/internal/core"
 	"github.com/testcontainers/testcontainers-go/log"
 	"github.com/testcontainers/testcontainers-go/wait"
@@ -240,7 +240,7 @@ func (c *ContainerRequest) GetContext() (io.Reader, error) {
 	}
 	c.Context = abs
 
-	dockerIgnoreExists, excluded, err := parseDockerIgnore(abs)
+	dockerIgnoreExists, excluded, err := image.ParseDockerIgnore(abs)
 	if err != nil {
 		return nil, err
 	}
@@ -261,26 +261,6 @@ func (c *ContainerRequest) GetContext() (io.Reader, error) {
 	}
 
 	return buildContext, nil
-}
-
-// parseDockerIgnore returns if the file exists, the excluded files and an error if any
-func parseDockerIgnore(targetDir string) (bool, []string, error) {
-	// based on https://github.com/docker/cli/blob/master/cli/command/image/build/dockerignore.go#L14
-	fileLocation := filepath.Join(targetDir, ".dockerignore")
-	var excluded []string
-	exists := false
-	if f, openErr := os.Open(fileLocation); openErr == nil {
-		defer f.Close()
-
-		exists = true
-
-		var err error
-		excluded, err = ignorefile.ReadAll(f)
-		if err != nil {
-			return true, excluded, fmt.Errorf("error reading .dockerignore: %w", err)
-		}
-	}
-	return exists, excluded, nil
 }
 
 // GetBuildArgs returns the env args to be used when creating from Dockerfile
---
+++ image/dockerignore_test.go
@@ -0,0 +1,30 @@
+package image
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestParseDockerIgnore(t *testing.T) {
+
+	assertions := func(t *testing.T, filePath string, exists bool, expectedErr error, expectedExcluded []string) {
+		ok, excluded, err := ParseDockerIgnore(filePath)
+		require.Equal(t, exists, ok)
+		require.Equal(t, expectedErr, err)
+		require.Equal(t, expectedExcluded, excluded)
+	}
+
+	t.Run("file-exists", func(t *testing.T) {
+		assertions(t, "./testdata/dockerignore", true, nil, []string{"vendor", "foo", "bar"})
+	})
+
+	t.Run("file-exists-including-comments", func(t *testing.T) {
+		assertions(t, "./testdata", true, nil, []string{"Dockerfile", "echo.Dockerfile"})
+	})
+
+	t.Run("file-does-not-exists", func(t *testing.T) {
+		assertions(t, "./testdata/data", false, nil, nil)
+	})
+
+}
