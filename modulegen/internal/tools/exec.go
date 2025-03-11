051ee3207d83fa4a4e3741c76dddeabfb22a45bf
Manuel de la Peña <mdelapenya@gmail.com>
2025-03-11 18:25:40 +0100

    chore: readd dependabot, including a way to refresh the project files for all the modules (#2997)
    * chore: bring back dependabot generation
    
    * chore: replace assert with require
    
    * feat: add a modulegen command to refresh the project files
    
    * chore: exclude compose from the mkdocs nav entries
    
    * fix: remove extra comma after refresh
    
    * chore: readd dependabot updates
    
    * fix: lint
    
    * chore: wrap errors in dependabot files
    
    * docs: document new types and functions
    
    * chore: simplify types merging generator and refresher
    
    * chore: include modulegen in dependabot
    
    * chore: check modulegen is included in the dependabot updates
    
    * docs: document new command
    
    * chore: exclude dependabot changes from the build pipeline
    
    * fix: lint
    
    * chore: wrap errors and add docs
    
    * chore: simplify dependabot config to use directories instead of one entry per Go module
    
    * chore: use filepath
    
    * chore: more wrap errors
    
    * docs: be explicit
    
    * chore: more filepaths to fix WIndows tests
    
    * fix: module path uses slashes
    
    * fix: test
    
    * chore: dependabot must follow unix file path separator

--- modulegen/internal/tools/exec.go
+++ modulegen/internal/tools/exec.go
@@ -5,35 +5,34 @@ import (
 	"os/exec"
 )
 
+// GoModTidy synchronizes the dependencies for a module or example,
+// running the `go mod tidy` command in the given directory.
 func GoModTidy(cmdDir string) error {
-	if err := runGoCommand(cmdDir, "mod", "tidy"); err != nil {
-		return fmt.Errorf(">> error synchronizing the dependencies: %w", err)
+	if err := runCommand(cmdDir, "go", "mod", "tidy"); err != nil {
+		return fmt.Errorf("mod tidy dependencies: %w", err)
 	}
 	return nil
 }
 
+// GoVet checks the generated code for errors,
+// running the `go vet ./...` command in the given directory.
 func GoVet(cmdDir string) error {
-	if err := runGoCommand(cmdDir, "vet", "./..."); err != nil {
-		return fmt.Errorf(">> error checking generated code: %w", err)
+	if err := runCommand(cmdDir, "go", "vet", "./..."); err != nil {
+		return fmt.Errorf("go vet generated code: %w", err)
 	}
 	return nil
 }
 
+// MakeLint runs the `make lint` command in the given directory.
 func MakeLint(cmdDir string) error {
-	if err := runMakeCommand(cmdDir, "lint"); err != nil {
-		return fmt.Errorf(">> error synchronizing the dependencies: %w", err)
+	if err := runCommand(cmdDir, "make", "lint"); err != nil {
+		return fmt.Errorf("make lint module: %w", err)
 	}
 	return nil
 }
 
-func runGoCommand(cmdDir string, args ...string) error {
-	cmd := exec.Command("go", args...)
-	cmd.Dir = cmdDir
-	return cmd.Run()
-}
-
-func runMakeCommand(cmdDir string, args ...string) error {
-	cmd := exec.Command("make", args...)
+func runCommand(cmdDir string, command string, args ...string) error {
+	cmd := exec.Command(command, args...)
 	cmd.Dir = cmdDir
 	return cmd.Run()
 }
