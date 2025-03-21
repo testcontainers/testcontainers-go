f23f143ae5f00349b051e6c5c465bfa9fec2f626
Manuel de la Peña <mdelapenya@gmail.com>
2025-03-21 11:19:50 +0100

    chore: exclude azurite deprecations from lint
    Services are now internal

--- modules/azurite/services.go
+++ modules/azurite/services.go
@@ -7,15 +7,25 @@ import (
 const (
 	// Deprecated: This constant is deprecated in favor of the one in "modules/azure/azurite".
 	// Please use that package instead for all new code.
+	//
+	//nolint:staticcheck
 	BlobService Service = azurite.BlobService
+
 	// Deprecated: This constant is deprecated in favor of the one in "modules/azure/azurite".
 	// Please use that package instead for all new code.
+	//
+	//nolint:staticcheck
 	QueueService Service = azurite.QueueService
+
 	// Deprecated: This constant is deprecated in favor of the one in "modules/azure/azurite".
 	// Please use that package instead for all new code.
+	//
+	//nolint:staticcheck
 	TableService Service = azurite.TableService
 )
 
 // Deprecated: This type is deprecated in favor of the one in "modules/azure/azurite".
 // Please use that package instead for all new code.
+//
+//nolint:staticcheck
 type Service = azurite.Service
