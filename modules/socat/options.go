055f790c00e3504719550463a84262621a7e0367
Manuel de la Peña <mdelapenya@gmail.com>
2025-03-21 07:49:08 +0100

    chore: implement socat

---
+++ modules/socat/options.go
@@ -0,0 +1,77 @@
+package socat
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/testcontainers/testcontainers-go"
+)
+
+type options struct {
+	// Targets is the map of targets of the socat container
+	Targets    map[int]Target
+	targetsCmd string
+}
+
+func defaultOptions() options {
+	return options{
+		Targets: map[int]Target{},
+	}
+}
+
+// Compiler check to ensure that Option implements the testcontainers.ContainerCustomizer interface.
+var _ testcontainers.ContainerCustomizer = (Option)(nil)
+
+// Option is an option for the Redpanda container.
+type Option func(*options)
+
+// Customize is a NOOP. It's defined to satisfy the testcontainers.ContainerCustomizer interface.
+func (o Option) Customize(*testcontainers.GenericContainerRequest) error {
+	// NOOP to satisfy interface.
+	return nil
+}
+
+// Target represents a target for the socat container.
+// Create a new target with NewTarget or NewTargetWithInternalPort.
+type Target struct {
+	exposedPort  int
+	internalPort int
+	host         string
+}
+
+func NewTarget(exposedPort int, host string) Target {
+	return Target{
+		exposedPort:  exposedPort,
+		internalPort: exposedPort,
+		host:         host,
+	}
+}
+
+func NewTargetWithInternalPort(exposedPort int, internalPort int, host string) Target {
+	return Target{
+		exposedPort:  exposedPort,
+		internalPort: internalPort,
+		host:         host,
+	}
+}
+
+// WithTargets sets the targets for the socat container.
+// The host of each target must be without the port, as it is internally mapped to the exposed port.
+func WithTargets(targets ...Target) Option {
+	return func(o *options) {
+		cmds := make([]string, 0, len(targets))
+
+		// If the internal port is not set, use the exposed port
+		for _, target := range targets {
+			if target.internalPort == 0 {
+				target.internalPort = target.exposedPort
+			}
+
+			o.Targets[target.exposedPort] = target
+
+			cmds = append(cmds, fmt.Sprintf("socat TCP-LISTEN:%d,fork,reuseaddr TCP:%s:%d", target.exposedPort, target.host, target.internalPort))
+		}
+
+		o.targetsCmd = strings.Join(cmds, " & ")
+	}
+}
--- modules/socat/socat_test.go
+++ modules/socat/socat_test.go
@@ -2,12 +2,15 @@ package socat_test
 
 import (
 	"context"
+	"io"
+	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/modules/socat"
+	"github.com/testcontainers/testcontainers-go/network"
 )
 
 func TestSocat(t *testing.T) {
@@ -18,4 +21,50 @@ func TestSocat(t *testing.T) {
 	require.NoError(t, err)
 
 	// perform assertions
+}
+
+func TestRun_helloWorld(t *testing.T) {
+	ctx := context.Background()
+
+	nw, err := network.New(ctx)
+	testcontainers.CleanupNetwork(t, nw)
+	require.NoError(t, err)
+
+	ctr, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: testcontainers.ContainerRequest{
+			Image:        "testcontainers/helloworld:1.2.0",
+			ExposedPorts: []string{"8080/tcp"},
+			Networks:     []string{nw.Name},
+			NetworkAliases: map[string][]string{
+				nw.Name: {"helloworld"},
+			},
+		},
+		Started: true,
+	})
+	testcontainers.CleanupContainer(t, ctr)
+	require.NoError(t, err)
+
+	target := socat.NewTarget(8080, "helloworld")
+
+	socatContainer, err := socat.Run(
+		ctx, "alpine/socat:1.8.0.1",
+		socat.WithTargets(target),
+		network.WithNetwork([]string{"socat"}, nw),
+	)
+	testcontainers.CleanupContainer(t, socatContainer)
+	require.NoError(t, err)
+
+	httpClient := http.DefaultClient
+
+	baseUri := socatContainer.TargetURL(target)
+	require.NotNil(t, baseUri)
+
+	resp, err := httpClient.Get(baseUri.String() + "/ping")
+	require.NoError(t, err)
+
+	require.Equal(t, 200, resp.StatusCode)
+
+	body, err := io.ReadAll(resp.Body)
+	require.NoError(t, err)
+	require.Equal(t, "PONG", string(body))
 }
