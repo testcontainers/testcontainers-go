2e7a830f0dc7f751899267c2ae6b876dac6eaf8a
Manuel de la Peña <mdelapenya@gmail.com>
2025-04-08 13:52:20 +0200

    chore: use new dockerconfig package instead of cpuguy83's

--- docker_auth_test.go
+++ docker_auth_test.go
@@ -7,94 +7,19 @@ import (
 	"fmt"
 	"net"
 	"os"
-	"path/filepath"
 	"testing"
 
-	"github.com/cpuguy83/dockercfg"
 	"github.com/docker/docker/api/types/image"
-	"github.com/docker/docker/api/types/registry"
 	"github.com/docker/docker/client"
 	"github.com/stretchr/testify/require"
 
-	"github.com/testcontainers/testcontainers-go/internal/core"
+	dockerconfig "github.com/testcontainers/testcontainers-go/internal/docker/config"
 	"github.com/testcontainers/testcontainers-go/wait"
 )
 
 const (
-	exampleAuth     = "https://example-auth.com"
-	privateRegistry = "https://my.private.registry"
-	exampleRegistry = "https://example.com"
+	exampleAuth = "https://example-auth.com"
 )
-
-func Test_getDockerConfig(t *testing.T) {
-	expectedConfig := &dockercfg.Config{
-		AuthConfigs: map[string]dockercfg.AuthConfig{
-			core.IndexDockerIO: {},
-			exampleRegistry:    {},
-			privateRegistry:    {},
-		},
-		CredentialsStore: "desktop",
-	}
-	t.Run("HOME/valid", func(t *testing.T) {
-		testDockerConfigHome(t, "testdata")
-
-		cfg, err := getDockerConfig()
-		require.NoError(t, err)
-		require.Equal(t, expectedConfig, cfg)
-	})
-
-	t.Run("HOME/not-found", func(t *testing.T) {
-		testDockerConfigHome(t, "testdata", "not-found")
-
-		cfg, err := getDockerConfig()
-		require.ErrorIs(t, err, os.ErrNotExist)
-		require.Nil(t, cfg)
-	})
-
-	t.Run("HOME/invalid-config", func(t *testing.T) {
-		testDockerConfigHome(t, "testdata", "invalid-config")
-
-		cfg, err := getDockerConfig()
-		require.ErrorContains(t, err, "json: cannot unmarshal array")
-		require.Nil(t, cfg)
-	})
-
-	t.Run("DOCKER_AUTH_CONFIG/valid", func(t *testing.T) {
-		testDockerConfigHome(t, "testdata", "not-found")
-		t.Setenv("DOCKER_AUTH_CONFIG", dockerConfig)
-
-		cfg, err := getDockerConfig()
-		require.NoError(t, err)
-		require.Equal(t, expectedConfig, cfg)
-	})
-
-	t.Run("DOCKER_AUTH_CONFIG/invalid-config", func(t *testing.T) {
-		testDockerConfigHome(t, "testdata", "not-found")
-		t.Setenv("DOCKER_AUTH_CONFIG", `{"auths": []}`)
-
-		cfg, err := getDockerConfig()
-		require.ErrorContains(t, err, "json: cannot unmarshal array")
-		require.Nil(t, cfg)
-	})
-
-	t.Run("DOCKER_CONFIG/valid", func(t *testing.T) {
-		testDockerConfigHome(t, "testdata", "not-found")
-		t.Setenv("DOCKER_CONFIG", filepath.Join("testdata", ".docker"))
-
-		cfg, err := getDockerConfig()
-		require.NoError(t, err)
-		require.Equal(t, expectedConfig, cfg)
-	})
-
-	t.Run("DOCKER_CONFIG/invalid-config", func(t *testing.T) {
-		testDockerConfigHome(t, "testdata", "not-found")
-		t.Setenv("DOCKER_CONFIG", filepath.Join("testdata", "invalid-config", ".docker"))
-
-		cfg, err := getDockerConfig()
-		require.ErrorContains(t, err, "json: cannot unmarshal array")
-		require.Nil(t, cfg)
-	})
-}
 
 func TestDockerImageAuth(t *testing.T) {
 	t.Run("retrieve auth with DOCKER_AUTH_CONFIG env var", func(t *testing.T) {
@@ -130,7 +55,7 @@ func TestDockerImageAuth(t *testing.T) {
 		setAuthConfig(t, invalidRegistryURL, "gopher", "secret")
 
 		registry, cfg, err := DockerImageAuth(context.Background(), imageReg+imagePath)
-		require.ErrorIs(t, err, dockercfg.ErrCredentialsNotFound)
+		require.ErrorIs(t, err, dockerconfig.ErrCredentialsNotFound)
 		require.Empty(t, cfg)
 		require.Equal(t, imageReg, registry)
 	})
@@ -150,7 +75,7 @@ func TestDockerImageAuth(t *testing.T) {
 		setAuthConfig(t, "example-auth.com", "gopher", "secret")
 
 		registry, cfg, err := DockerImageAuth(context.Background(), imageReg+imagePath)
-		require.ErrorIs(t, err, dockercfg.ErrCredentialsNotFound)
+		require.ErrorIs(t, err, dockerconfig.ErrCredentialsNotFound)
 		require.Empty(t, cfg)
 		require.Equal(t, imageReg, registry)
 	})
@@ -377,142 +302,4 @@ func localAddress(t *testing.T) string {
 	localAddr := conn.LocalAddr().(*net.UDPAddr)
 
 	return localAddr.IP.String()
-}
-
-//go:embed testdata/.docker/config.json
-var dockerConfig string
-
-// reset resets the credentials cache.
-func (c *credentialsCache) reset() {
-	c.mtx.Lock()
-	defer c.mtx.Unlock()
-	c.entries = make(map[string]credentials)
-}
-
-func Test_getDockerAuthConfigs(t *testing.T) {
-	t.Run("HOME/valid", func(t *testing.T) {
-		testDockerConfigHome(t, "testdata")
-
-		requireValidAuthConfig(t)
-	})
-
-	t.Run("HOME/not-found", func(t *testing.T) {
-		testDockerConfigHome(t, "testdata", "not-exist")
-
-		authConfigs, err := getDockerAuthConfigs()
-		require.NoError(t, err)
-		require.NotNil(t, authConfigs)
-		require.Empty(t, authConfigs)
-	})
-
-	t.Run("HOME/invalid-config", func(t *testing.T) {
-		testDockerConfigHome(t, "testdata", "invalid-config")
-
-		authConfigs, err := getDockerAuthConfigs()
-		require.ErrorContains(t, err, "json: cannot unmarshal array")
-		require.Nil(t, authConfigs)
-	})
-
-	t.Run("DOCKER_AUTH_CONFIG/valid", func(t *testing.T) {
-		testDockerConfigHome(t, "testdata", "not-exist")
-		t.Setenv("DOCKER_AUTH_CONFIG", dockerConfig)
-
-		requireValidAuthConfig(t)
-	})
-
-	t.Run("DOCKER_AUTH_CONFIG/invalid-config", func(t *testing.T) {
-		testDockerConfigHome(t, "testdata", "not-exist")
-		t.Setenv("DOCKER_AUTH_CONFIG", `{"auths": []}`)
-
-		authConfigs, err := getDockerAuthConfigs()
-		require.ErrorContains(t, err, "json: cannot unmarshal array")
-		require.Nil(t, authConfigs)
-	})
-
-	t.Run("DOCKER_AUTH_CONFIG/identity-token", func(t *testing.T) {
-		testDockerConfigHome(t, "testdata", "not-exist")
-
-		// Reset the credentials cache to ensure our mocked method is called.
-		creds.reset()
-
-		// Mock getRegistryCredentials to return identity-token for index.docker.io.
-		old := getRegistryCredentials
-		t.Cleanup(func() {
-			getRegistryCredentials = old
-			creds.reset() // Ensure our mocked results aren't cached.
-		})
-		getRegistryCredentials = func(hostname string) (string, string, error) {
-			switch hostname {
-			case core.IndexDockerIO:
-				return "", "identity-token", nil
-			default:
-				return "username", "password", nil
-			}
-		}
-		t.Setenv("DOCKER_AUTH_CONFIG", dockerConfig)
-
-		authConfigs, err := getDockerAuthConfigs()
-		require.NoError(t, err)
-		require.Equal(t, map[string]registry.AuthConfig{
-			core.IndexDockerIO: {
-				IdentityToken: "identity-token",
-			},
-			privateRegistry: {
-				Username: "username",
-				Password: "password",
-			},
-			exampleRegistry: {
-				Username: "username",
-				Password: "password",
-			},
-		}, authConfigs)
-	})
-
-	t.Run("DOCKER_CONFIG/valid", func(t *testing.T) {
-		testDockerConfigHome(t, "testdata", "not-found")
-		t.Setenv("DOCKER_CONFIG", filepath.Join("testdata", ".docker"))
-
-		requireValidAuthConfig(t)
-	})
-
-	t.Run("DOCKER_CONFIG/invalid-config", func(t *testing.T) {
-		testDockerConfigHome(t, "testdata", "not-found")
-		t.Setenv("DOCKER_CONFIG", filepath.Join("testdata", "invalid-config", ".docker"))
-
-		cfg, err := getDockerConfig()
-		require.ErrorContains(t, err, "json: cannot unmarshal array")
-		require.Nil(t, cfg)
-	})
-}
-
-// requireValidAuthConfig checks that the given authConfigs map contains the expected keys.
-func requireValidAuthConfig(t *testing.T) {
-	t.Helper()
-
-	authConfigs, err := getDockerAuthConfigs()
-	require.NoError(t, err)
-
-	// We can only check the keys as the values are not deterministic as they depend
-	// on user's environment.
-	expected := map[string]registry.AuthConfig{
-		core.IndexDockerIO: {},
-		exampleRegistry:    {},
-		privateRegistry:    {},
-	}
-	for k := range authConfigs {
-		authConfigs[k] = registry.AuthConfig{}
-	}
-	require.Equal(t, expected, authConfigs)
-}
-
-// testDockerConfigHome sets the user's home directory to the given path
-// and unsets the DOCKER_CONFIG and DOCKER_AUTH_CONFIG environment variables.
-func testDockerConfigHome(t *testing.T, dirs ...string) {
-	t.Helper()
-
-	dir := filepath.Join(dirs...)
-	t.Setenv("DOCKER_AUTH_CONFIG", "")
-	t.Setenv("DOCKER_CONFIG", "")
-	t.Setenv("HOME", dir)
-	t.Setenv("USERPROFILE", dir) // Windows
 }
