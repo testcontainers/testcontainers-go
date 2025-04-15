9e11c9ed35e08d7aec0b1c029714f4706a712bd3
Manuel de la Peña <mdelapenya@gmail.com>
2025-04-15 22:09:11 +0200

    chore: return nil

--- modules/dockermodelrunner/sdk/client/create.go
+++ modules/dockermodelrunner/sdk/client/create.go
@@ -10,31 +10,29 @@ import (
 
 // CreateModel creates a model
 func (c *Client) CreateModel(ctx context.Context, fqmn string) ([]byte, error) {
-	var bytes []byte
-
 	payload := fmt.Sprintf(`{"from": "%q"}`, fqmn)
 	reqURL := c.baseURL + "/models/create"
 
 	req, err := http.NewRequestWithContext(ctx, http.MethodPost, reqURL, strings.NewReader(payload))
 	if err != nil {
-		return bytes, fmt.Errorf("new post request (%s): %w", reqURL, err)
+		return nil, fmt.Errorf("new post request (%s): %w", reqURL, err)
 	}
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return bytes, fmt.Errorf("http post: %w", err)
+		return nil, fmt.Errorf("http post: %w", err)
 	}
 	defer resp.Body.Close()
 
 	// The Docker Model Runner returns a 200 status code for a successful pull
 	if resp.StatusCode != http.StatusOK {
-		return bytes, fmt.Errorf("status code: %d", resp.StatusCode)
+		return nil, fmt.Errorf("status code: %d", resp.StatusCode)
 	}
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return bytes, fmt.Errorf("read all: %w", err)
+		return nil, fmt.Errorf("read all: %w", err)
 	}
-	bytes = body
-	return bytes, nil
+
+	return body, nil
 }
