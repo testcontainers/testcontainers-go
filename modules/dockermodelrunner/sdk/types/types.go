a846e3cabfbaa27b687927c81830fc49a4d53f80
Manuel de la Peña <mdelapenya@gmail.com>
2025-04-16 08:25:49 +0200

    chore: use time.Time for model created field

--- modules/dockermodelrunner/sdk/types/types.go
+++ modules/dockermodelrunner/sdk/types/types.go
@@ -1,5 +1,10 @@
 package types
 
+import (
+	"encoding/json"
+	"time"
+)
+
 // ModelResponse is the response for a model
 type ModelResponse struct {
 	// ID is the ID of the model
@@ -12,5 +17,28 @@ type ModelResponse struct {
 	Config map[string]string `json:"config"`
 
 	// Created is the creation time of the model
-	Created int64 `json:"created"`
+	Created JSONTime `json:"created"`
+}
+
+// JSONTime handles Unix timestamp conversion to/from time.Time
+type JSONTime time.Time
+
+// UnmarshalJSON implements json.Unmarshaler
+func (j *JSONTime) UnmarshalJSON(b []byte) error {
+	var timestamp int64
+	if err := json.Unmarshal(b, &timestamp); err != nil {
+		return err
+	}
+	*j = JSONTime(time.Unix(timestamp, 0))
+	return nil
+}
+
+// MarshalJSON implements json.Marshaler
+func (j JSONTime) MarshalJSON() ([]byte, error) {
+	return json.Marshal(time.Time(j).Unix())
+}
+
+// Time returns the time.Time representation
+func (j JSONTime) Time() time.Time {
+	return time.Time(j)
 }
