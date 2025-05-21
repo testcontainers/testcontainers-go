95711be7ab2956d2d8a129cff5ef6caafc60e36d
Manuel de la Peña <mdelapenya@gmail.com>
2025-05-22 01:54:10 +0200

    break(mssql): use testcontainers.Run

--- modules/mssql/mssql.go
+++ modules/mssql/mssql.go
@@ -4,12 +4,10 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"io"
 	"strings"
 	"time"
 
 	"github.com/testcontainers/testcontainers-go"
-	tcexec "github.com/testcontainers/testcontainers-go/exec"
 	"github.com/testcontainers/testcontainers-go/wait"
 )
 
@@ -31,79 +29,6 @@ func (c *MSSQLServerContainer) Password() string {
 	return c.password
 }
 
-// WithAcceptEULA sets the ACCEPT_EULA environment variable to "Y"
-func WithAcceptEULA() testcontainers.CustomizeRequestOption {
-	return func(req *testcontainers.GenericContainerRequest) error {
-		req.Env["ACCEPT_EULA"] = "Y"
-
-		return nil
-	}
-}
-
-// WithPassword sets the MSSQL_SA_PASSWORD environment variable to the provided password
-func WithPassword(password string) testcontainers.CustomizeRequestOption {
-	return func(req *testcontainers.GenericContainerRequest) error {
-		if password == "" {
-			password = defaultPassword
-		}
-		req.Env["MSSQL_SA_PASSWORD"] = password
-
-		return nil
-	}
-}
-
-// WithInitSQL adds SQL scripts to be executed after the container is ready.
-// The scripts are executed in the order they are provided using sqlcmd tool.
-func WithInitSQL(files ...io.Reader) testcontainers.CustomizeRequestOption {
-	return func(req *testcontainers.GenericContainerRequest) error {
-		hooks := make([]testcontainers.ContainerHook, 0, len(files))
-
-		for i, script := range files {
-			content, err := io.ReadAll(script)
-			if err != nil {
-				return fmt.Errorf("failed to read script: %w", err)
-			}
-
-			hook := func(ctx context.Context, c testcontainers.Container) error {
-				password := defaultPassword
-				if req.Env["MSSQL_SA_PASSWORD"] != "" {
-					password = req.Env["MSSQL_SA_PASSWORD"]
-				}
-
-				// targetPath is a dummy path to store the script in the container
-				targetPath := "/tmp/" + fmt.Sprintf("script_%d.sql", i)
-				if err := c.CopyToContainer(ctx, content, targetPath, 0o644); err != nil {
-					return fmt.Errorf("failed to copy script to container: %w", err)
-				}
-
-				// NOTE: we add both legacy and new mssql-tools paths to ensure compatibility
-				envOpts := tcexec.WithEnv([]string{
-					"PATH=/opt/mssql-tools18/bin:/opt/mssql-tools/bin:$PATH",
-				})
-				cmd := []string{
-					"sqlcmd",
-					"-S", "localhost",
-					"-U", defaultUsername,
-					"-P", password,
-					"-No",
-					"-i", targetPath,
-				}
-				if _, _, err := c.Exec(ctx, cmd, envOpts); err != nil {
-					return fmt.Errorf("failed to execute SQL script %q using sqlcmd: %w", targetPath, err)
-				}
-				return nil
-			}
-			hooks = append(hooks, hook)
-		}
-
-		req.LifecycleHooks = append(req.LifecycleHooks, testcontainers.ContainerLifecycleHooks{
-			PostReadies: hooks,
-		})
-
-		return nil
-	}
-}
-
 // Deprecated: use Run instead
 // RunContainer creates an instance of the MSSQLServer container type
 func RunContainer(ctx context.Context, opts ...testcontainers.ContainerCustomizer) (*MSSQLServerContainer, error) {
@@ -112,41 +37,39 @@ func RunContainer(ctx context.Context, opts ...testcontainers.ContainerCustomize
 
 // Run creates an instance of the MSSQLServer container type
 func Run(ctx context.Context, img string, opts ...testcontainers.ContainerCustomizer) (*MSSQLServerContainer, error) {
-	req := testcontainers.ContainerRequest{
-		Image:        img,
-		ExposedPorts: []string{defaultPort},
-		Env: map[string]string{
-			"MSSQL_SA_PASSWORD": defaultPassword,
-		},
-		WaitingFor: wait.ForAll(
+	moduleOpts := []testcontainers.ContainerCustomizer{
+		testcontainers.WithExposedPorts(defaultPort),
+		testcontainers.WithWaitStrategy(wait.ForAll(
 			wait.ForListeningPort(defaultPort).WithStartupTimeout(time.Minute),
 			wait.ForLog("Recovery is complete."),
-		),
+		)),
 	}
 
-	genericContainerReq := testcontainers.GenericContainerRequest{
-		ContainerRequest: req,
-		Started:          true,
-	}
+	moduleOpts = append(moduleOpts, opts...)
 
+	defaultSettings := defaultOptions()
 	for _, opt := range opts {
-		if err := opt.Customize(&genericContainerReq); err != nil {
-			return nil, fmt.Errorf("customize: %w", err)
+		if o, ok := opt.(Option); ok {
+			if err := o(&defaultSettings); err != nil {
+				return nil, fmt.Errorf("mssql option: %w", err)
+			}
 		}
 	}
 
-	if strings.ToUpper(genericContainerReq.Env["ACCEPT_EULA"]) != "Y" {
+	if strings.ToUpper(defaultSettings.env["ACCEPT_EULA"]) != "Y" {
 		return nil, errors.New("EULA not accepted. Please use the WithAcceptEULA option to accept the EULA")
 	}
 
-	container, err := testcontainers.GenericContainer(ctx, genericContainerReq)
+	moduleOpts = append(moduleOpts, testcontainers.WithEnv(defaultSettings.env))
+
+	ctr, err := testcontainers.Run(ctx, img, moduleOpts...)
 	var c *MSSQLServerContainer
-	if container != nil {
-		c = &MSSQLServerContainer{Container: container, password: req.Env["MSSQL_SA_PASSWORD"], username: defaultUsername}
+	if ctr != nil {
+		c = &MSSQLServerContainer{Container: ctr, password: defaultSettings.env["MSSQL_SA_PASSWORD"], username: defaultUsername}
 	}
 
 	if err != nil {
-		return c, fmt.Errorf("generic container: %w", err)
+		return c, fmt.Errorf("run: %w", err)
 	}
 
 	return c, nil
