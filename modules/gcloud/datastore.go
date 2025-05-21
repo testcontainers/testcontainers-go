92a00b4cf9b0bcd2fd7aee36e332446da517581f
Manuel de la Peña <mdelapenya@gmail.com>
2025-05-21 14:32:02 +0200

    chore(gcloud): use testcontainers.Run in deprecated constructors

--- modules/gcloud/datastore.go
+++ modules/gcloud/datastore.go
@@ -16,25 +16,23 @@ func RunDatastoreContainer(ctx context.Context, opts ...testcontainers.Container
 // Deprecated: use [datastore.Run] instead
 // RunDatastore creates an instance of the GCloud container type for Datastore.
 func RunDatastore(ctx context.Context, img string, opts ...testcontainers.ContainerCustomizer) (*GCloudContainer, error) {
-	req := testcontainers.GenericContainerRequest{
-		ContainerRequest: testcontainers.ContainerRequest{
-			Image:        img,
-			ExposedPorts: []string{"8081/tcp"},
-			WaitingFor:   wait.ForHTTP("/").WithPort("8081/tcp"),
-		},
-		Started: true,
+	moduleOpts := []testcontainers.ContainerCustomizer{
+		testcontainers.WithExposedPorts("8081/tcp"),
+		testcontainers.WithWaitStrategy(wait.ForHTTP("/").WithPort("8081/tcp")),
 	}
 
-	settings, err := applyOptions(&req, opts)
+	moduleOpts = append(moduleOpts, opts...)
+
+	settings, err := applyOptions(opts)
 	if err != nil {
 		return nil, err
 	}
 
-	req.Cmd = []string{
+	moduleOpts = append(moduleOpts, testcontainers.WithCmd(
 		"/bin/sh",
 		"-c",
-		"gcloud beta emulators datastore start --host-port 0.0.0.0:8081 --project=" + settings.ProjectID,
-	}
+		"gcloud beta emulators datastore start --host-port 0.0.0.0:8081 --project="+settings.ProjectID,
+	))
 
-	return newGCloudContainer(ctx, req, 8081, settings, "")
+	return newGCloudContainer(ctx, img, 8081, settings, "", moduleOpts...)
 }
