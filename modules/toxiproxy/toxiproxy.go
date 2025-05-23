cfc3b1d88cbcf32fc872bf3d0400255cdb1e5b0d
Manuel de la Peña <mdelapenya@gmail.com>
2025-05-23 18:41:46 +0200

    chore(toxiproxy): use testcontainers.Run

--- modules/toxiproxy/toxiproxy.go
+++ modules/toxiproxy/toxiproxy.go
@@ -54,28 +54,21 @@ func (c *Container) URI(ctx context.Context) (string, error) {
 
 // Run creates an instance of the Toxiproxy container type
 func Run(ctx context.Context, img string, opts ...testcontainers.ContainerCustomizer) (*Container, error) {
-	req := testcontainers.ContainerRequest{
-		Image:        img,
-		ExposedPorts: []string{ControlPort},
-		WaitingFor: wait.ForHTTP("/version").WithPort(ControlPort).WithStatusCodeMatcher(func(status int) bool {
+	moduleOpts := []testcontainers.ContainerCustomizer{
+		testcontainers.WithExposedPorts(ControlPort),
+		testcontainers.WithWaitStrategy(wait.ForHTTP("/version").WithPort(ControlPort).WithStatusCodeMatcher(func(status int) bool {
 			return status == http.StatusOK
-		}),
+		})),
 	}
 
-	genericContainerReq := testcontainers.GenericContainerRequest{
-		ContainerRequest: req,
-		Started:          true,
-	}
+	moduleOpts = append(moduleOpts, opts...)
 
 	settings := defaultOptions()
 	for _, opt := range opts {
 		if apply, ok := opt.(Option); ok {
 			if err := apply(&settings); err != nil {
-				return nil, fmt.Errorf("apply: %w", err)
+				return nil, fmt.Errorf("toxiproxy option: %w", err)
 			}
-		}
-		if err := opt.Customize(&genericContainerReq); err != nil {
-			return nil, fmt.Errorf("customize: %w", err)
 		}
 	}
 
@@ -87,7 +80,7 @@ func Run(ctx context.Context, img string, opts ...testcontainers.ContainerCustom
 		proxy.Listen = fmt.Sprintf("0.0.0.0:%d", proxiedPort)
 		portsInRange = append(portsInRange, fmt.Sprintf("%d/tcp", proxiedPort))
 	}
-	genericContainerReq.ExposedPorts = append(genericContainerReq.ExposedPorts, portsInRange...)
+	moduleOpts = append(moduleOpts, testcontainers.WithExposedPorts(portsInRange...))
 
 	// Render the config file
 	jsonData, err := json.MarshalIndent(settings.proxies, "", "    ")
@@ -97,22 +90,22 @@ func Run(ctx context.Context, img string, opts ...testcontainers.ContainerCustom
 
 	// Apply the config file to the container with the proxies.
 	if len(settings.proxies) > 0 {
-		genericContainerReq.Files = append(genericContainerReq.Files, testcontainers.ContainerFile{
+		moduleOpts = append(moduleOpts, testcontainers.WithFiles(testcontainers.ContainerFile{
 			Reader:            bytes.NewReader(jsonData),
 			ContainerFilePath: "/tmp/tc-toxiproxy.json",
 			FileMode:          0o644,
-		})
-		genericContainerReq.Cmd = append(genericContainerReq.Cmd, "-host=0.0.0.0", "-config=/tmp/tc-toxiproxy.json")
+		}))
+		moduleOpts = append(moduleOpts, testcontainers.WithCmdArgs("-host=0.0.0.0", "-config=/tmp/tc-toxiproxy.json"))
 	}
 
-	container, err := testcontainers.GenericContainer(ctx, genericContainerReq)
+	ctr, err := testcontainers.Run(ctx, img, moduleOpts...)
 	var c *Container
-	if container != nil {
-		c = &Container{Container: container, proxiedEndpoints: make(map[int]string)}
+	if ctr != nil {
+		c = &Container{Container: ctr, proxiedEndpoints: make(map[int]string)}
 	}
 
 	if err != nil {
-		return c, fmt.Errorf("generic container: %w", err)
+		return c, fmt.Errorf("run: %w", err)
 	}
 
 	// Map the ports of the proxies to the container, so that we can use them in the tests
