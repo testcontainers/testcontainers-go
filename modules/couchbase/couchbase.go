b6461e8029575190a970840441f0508415f3f877
Sean Doyle <83722846+waroir20@users.noreply.github.com>
2025-05-30 07:53:22 -0500

    feat(couchbase): adding auth to couchbase initCluster functions to support container reuse (#3048)
    * Adding auth to couchbase initCluster functions to support container reuse
    
    * chore: use new functional option instead of custom code
    
    * fix: container name must be simpler
    
    ---------
    
    Co-authored-by: Manuel de la Peña <mdelapenya@gmail.com>

--- modules/couchbase/couchbase.go
+++ modules/couchbase/couchbase.go
@@ -219,11 +219,12 @@ func (c *CouchbaseContainer) waitUntilNodeIsOnline(ctx context.Context) error {
 		WithStatusCodeMatcher(func(status int) bool {
 			return status == http.StatusOK
 		}).
+		WithBasicAuth(c.config.username, c.config.password).
 		WaitUntilReady(ctx, c)
 }
 
 func (c *CouchbaseContainer) initializeIsEnterprise(ctx context.Context) error {
-	response, err := c.doHTTPRequest(ctx, MGMT_PORT, "/pools", http.MethodGet, nil, false)
+	response, err := c.doHTTPRequest(ctx, MGMT_PORT, "/pools", http.MethodGet, nil)
 	if err != nil {
 		return err
 	}
@@ -252,7 +253,7 @@ func (c *CouchbaseContainer) renameNode(ctx context.Context) error {
 		"hostname": hostname,
 	}
 
-	_, err = c.doHTTPRequest(ctx, MGMT_PORT, "/node/controller/rename", http.MethodPost, body, false)
+	_, err = c.doHTTPRequest(ctx, MGMT_PORT, "/node/controller/rename", http.MethodPost, body)
 
 	return err
 }
@@ -261,7 +262,7 @@ func (c *CouchbaseContainer) initializeServices(ctx context.Context) error {
 	body := map[string]string{
 		"services": c.getEnabledServices(),
 	}
-	_, err := c.doHTTPRequest(ctx, MGMT_PORT, "/node/controller/setupServices", http.MethodPost, body, false)
+	_, err := c.doHTTPRequest(ctx, MGMT_PORT, "/node/controller/setupServices", http.MethodPost, body)
 
 	return err
 }
@@ -282,7 +283,7 @@ func (c *CouchbaseContainer) setMemoryQuotas(ctx context.Context) error {
 		}
 	}
 
-	_, err := c.doHTTPRequest(ctx, MGMT_PORT, "/pools/default", http.MethodPost, body, false)
+	_, err := c.doHTTPRequest(ctx, MGMT_PORT, "/pools/default", http.MethodPost, body)
 
 	return err
 }
@@ -294,7 +295,7 @@ func (c *CouchbaseContainer) configureAdminUser(ctx context.Context) error {
 		"port":     "SAME",
 	}
 
-	_, err := c.doHTTPRequest(ctx, MGMT_PORT, "/settings/web", http.MethodPost, body, false)
+	_, err := c.doHTTPRequest(ctx, MGMT_PORT, "/settings/web", http.MethodPost, body)
 
 	return err
 }
@@ -353,7 +354,7 @@ func (c *CouchbaseContainer) configureExternalPorts(ctx context.Context) error {
 		body["eventingSSL"] = eventingSSL.Port()
 	}
 
-	_, err := c.doHTTPRequest(ctx, MGMT_PORT, "/node/controller/setupAlternateAddresses/external", http.MethodPut, body, true)
+	_, err := c.doHTTPRequest(ctx, MGMT_PORT, "/node/controller/setupAlternateAddresses/external", http.MethodPut, body)
 
 	return err
 }
@@ -371,7 +372,7 @@ func (c *CouchbaseContainer) configureIndexer(ctx context.Context) error {
 		"storageMode": string(c.config.indexStorageMode),
 	}
 
-	_, err := c.doHTTPRequest(ctx, MGMT_PORT, "/settings/indexes", http.MethodPost, body, true)
+	_, err := c.doHTTPRequest(ctx, MGMT_PORT, "/settings/indexes", http.MethodPost, body)
 
 	return err
 }
@@ -474,7 +475,7 @@ func (c *CouchbaseContainer) isPrimaryIndexOnline(ctx context.Context, bucket bu
 	}
 
 	err := backoff.Retry(func() error {
-		response, err := c.doHTTPRequest(ctx, QUERY_PORT, "/query/service", http.MethodPost, body, true)
+		response, err := c.doHTTPRequest(ctx, QUERY_PORT, "/query/service", http.MethodPost, body)
 		if err != nil {
 			return err
 		}
@@ -495,7 +496,7 @@ func (c *CouchbaseContainer) createPrimaryIndex(ctx context.Context, bucket buck
 		"statement": "CREATE PRIMARY INDEX on `" + bucket.name + "`",
 	}
 	err := backoff.Retry(func() error {
-		response, err := c.doHTTPRequest(ctx, QUERY_PORT, "/query/service", http.MethodPost, body, true)
+		response, err := c.doHTTPRequest(ctx, QUERY_PORT, "/query/service", http.MethodPost, body)
 		firstError := gjson.Get(string(response), "errors.0.code").Int()
 		if firstError != 0 {
 			return errors.New("index creation failed")
@@ -511,7 +512,7 @@ func (c *CouchbaseContainer) isQueryKeyspacePresent(ctx context.Context, bucket 
 	}
 
 	err := backoff.Retry(func() error {
-		response, err := c.doHTTPRequest(ctx, QUERY_PORT, "/query/service", http.MethodPost, body, true)
+		response, err := c.doHTTPRequest(ctx, QUERY_PORT, "/query/service", http.MethodPost, body)
 		if err != nil {
 			return err
 		}
@@ -557,12 +558,12 @@ func (c *CouchbaseContainer) createBucket(ctx context.Context, bucket bucket) er
 		"replicaNumber": strconv.Itoa(bucket.numReplicas),
 	}
 
-	_, err := c.doHTTPRequest(ctx, MGMT_PORT, "/pools/default/buckets", http.MethodPost, body, true)
+	_, err := c.doHTTPRequest(ctx, MGMT_PORT, "/pools/default/buckets", http.MethodPost, body)
 
 	return err
 }
 
-func (c *CouchbaseContainer) doHTTPRequest(ctx context.Context, port, path, method string, body map[string]string, auth bool) ([]byte, error) {
+func (c *CouchbaseContainer) doHTTPRequest(ctx context.Context, port, path, method string, body map[string]string) ([]byte, error) {
 	form := url.Values{}
 	for k, v := range body {
 		form.Set(k, v)
@@ -582,10 +583,7 @@ func (c *CouchbaseContainer) doHTTPRequest(ctx context.Context, port, path, meth
 		}
 
 		request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-
-		if auth {
-			request.SetBasicAuth(c.config.username, c.config.password)
-		}
+		request.SetBasicAuth(c.config.username, c.config.password)
 
 		response, err := http.DefaultClient.Do(request)
 		if err != nil {
