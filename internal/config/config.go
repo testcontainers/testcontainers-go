4244b04e40260b96af8d81de0faff4dbe0b60348
Manuel de la Peña <mdelapenya@gmail.com>
2025-06-02 16:29:23 +0200

    chore: bump ryuk to 0.12.0 (#3195)

--- internal/config/config.go
+++ internal/config/config.go
@@ -11,7 +11,7 @@ import (
 	"github.com/magiconair/properties"
 )
 
-const ReaperDefaultImage = "testcontainers/ryuk:0.11.0"
+const ReaperDefaultImage = "testcontainers/ryuk:0.12.0"
 
 var (
 	tcConfig     Config
