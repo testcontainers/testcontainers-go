1ab1dc4a32be3dad6cfdec8531f2dd22e23e85b8
Elchin Gasimov <egasimov0@gmail.com>
2025-08-25 21:23:02 +0400

    Remove static container name

--- modules/nebulagraph/options.go
+++ modules/nebulagraph/options.go
@@ -2,6 +2,7 @@ package nebulagraph
 
 import (
 	_ "embed"
+	"fmt"
 	"github.com/testcontainers/testcontainers-go/network"
 	"time"
 
@@ -12,18 +13,16 @@ import (
 //go:embed activator.sh
 var activatorScript string
 
+const user = "root"
+
 const (
 	defaultNebulaConsoleImage = "vesoft/nebula-console:v3.8.0"
-
-	defaultGraphdImage   = "vesoft/nebula-graphd:v3.8.0"
-	defaultMetadImage    = "vesoft/nebula-metad:v3.8.0"
-	defaultStoragedImage = "vesoft/nebula-storaged:v3.8.0"
 )
 
 const (
-	defaultMetadContainerName    = "metad0"
-	defaultGraphdContainerName   = "graphd0"
-	defaultStoragedContainerName = "storaged0"
+	metadNetworkAlias    = "metad0"
+	graphdNetworkAlias   = "graphd0"
+	storagedNetworkAlias = "storaged0"
 )
 
 const (
@@ -38,34 +37,32 @@ const (
 
 func defaultGrapdContainerCustomizers(nw *testcontainers.DockerNetwork) []testcontainers.ContainerCustomizer {
 	customizers := []testcontainers.ContainerCustomizer{
-		testcontainers.WithName(defaultGraphdContainerName),
 		testcontainers.WithExposedPorts(graphdPort+"/tcp", graphdPortHTTP+"/tcp"),
 		testcontainers.WithCmdArgs([]string{
-			"--meta_server_addrs=metad0:" + metadPort,
+			"--meta_server_addrs=" + metadNetworkAlias + ":" + metadPort,
 			"--port=" + graphdPort,
-			"--local_ip=graphd0",
-			"--ws_ip=graphd0",
+			"--local_ip=" + graphdNetworkAlias,
+			"--ws_ip=" + graphdNetworkAlias,
 			"--ws_http_port=" + graphdPortHTTP,
 			"--logtostderr=true",
 			"--redirect_stdout=false",
 			"--v=0",
 			"--minloglevel=0",
 		}...),
-		testcontainers.WithEnv(map[string]string{"USER": "root"}),
+		testcontainers.WithEnv(map[string]string{"USER": user}),
 		testcontainers.WithWaitStrategy(wait.ForHTTP("/status").WithPort(graphdPortHTTP + "/tcp").WithStartupTimeout(2 * time.Minute)),
-		network.WithNetwork([]string{"graphd0"}, nw),
+		network.WithNetwork([]string{graphdNetworkAlias}, nw),
 	}
 	return customizers
 }
 
 func defaultMetadContainerCustomizers(nw *testcontainers.DockerNetwork) []testcontainers.ContainerCustomizer {
 	customizers := []testcontainers.ContainerCustomizer{
-		testcontainers.WithName(defaultMetadContainerName),
 		testcontainers.WithExposedPorts(metadPort+"/tcp", metadPortHTTP+"/tcp"),
 		testcontainers.WithCmdArgs([]string{
-			"--meta_server_addrs=metad0:" + metadPort,
-			"--local_ip=metad0",
-			"--ws_ip=metad0",
+			"--meta_server_addrs=" + metadNetworkAlias + ":" + metadPort,
+			"--local_ip=" + metadNetworkAlias,
+			"--ws_ip=" + metadNetworkAlias,
 			"--port=" + metadPort,
 			"--ws_http_port=" + metadPortHTTP,
 			"--data_path=/data/meta",
@@ -74,21 +71,20 @@ func defaultMetadContainerCustomizers(nw *testcontainers.DockerNetwork) []testco
 			"--v=0",
 			"--minloglevel=0",
 		}...),
-		testcontainers.WithEnv(map[string]string{"USER": "root"}),
+		testcontainers.WithEnv(map[string]string{"USER": user}),
 		testcontainers.WithWaitStrategy(wait.ForHTTP("/status").WithPort(metadPortHTTP + "/tcp").WithStartupTimeout(2 * time.Minute)),
-		network.WithNetwork([]string{"metad0"}, nw),
+		network.WithNetwork([]string{metadNetworkAlias}, nw),
 	}
 	return customizers
 }
 
 func defaultStoragedContainerCustomizers(nw *testcontainers.DockerNetwork) []testcontainers.ContainerCustomizer {
 	customizers := []testcontainers.ContainerCustomizer{
-		testcontainers.WithName(defaultStoragedContainerName),
 		testcontainers.WithExposedPorts(storagedPort+"/tcp", storagedPortHTTP+"/tcp"),
 		testcontainers.WithCmdArgs([]string{
-			"--meta_server_addrs=metad0:" + metadPort,
-			"--local_ip=storaged0",
-			"--ws_ip=storaged0",
+			"--meta_server_addrs=" + metadNetworkAlias + ":" + metadPort,
+			"--local_ip=" + storagedNetworkAlias,
+			"--ws_ip=" + storagedNetworkAlias,
 			"--port=" + storagedPort,
 			"--ws_http_port=" + storagedPortHTTP,
 			"--data_path=/data/storage",
@@ -97,16 +93,17 @@ func defaultStoragedContainerCustomizers(nw *testcontainers.DockerNetwork) []tes
 			"--v=0",
 			"--minloglevel=0",
 		}...),
-		testcontainers.WithEnv(map[string]string{"USER": "root"}),
-		testcontainers.WithWaitStrategy(wait.ForLog("localhost = \"storaged0\":" + storagedPort).WithStartupTimeout(30 * time.Second)),
-		network.WithNetwork([]string{"storaged0"}, nw),
+		testcontainers.WithEnv(map[string]string{"USER": user}),
+		testcontainers.WithWaitStrategy(
+			wait.ForLog(fmt.Sprintf(`localhost = "%s":%s`, storagedNetworkAlias, storagedPort)).WithStartupTimeout(30 * time.Second),
+		),
+		network.WithNetwork([]string{storagedNetworkAlias}, nw),
 	}
 	return customizers
 }
 
 func defaultActivatorContainerCustomizers(nw *testcontainers.DockerNetwork) []testcontainers.ContainerCustomizer {
 	customizers := []testcontainers.ContainerCustomizer{
-		testcontainers.WithName("activator"),
 		testcontainers.WithEntrypoint([]string{}...),
 		testcontainers.WithCmd([]string{
 			"sh", "-c",
@@ -114,10 +111,10 @@ func defaultActivatorContainerCustomizers(nw *testcontainers.DockerNetwork) []te
 		}...),
 		testcontainers.WithExposedPorts(),
 		testcontainers.WithEnv(map[string]string{
-			"USER":            "root",
+			"USER":            user,
 			"ACTIVATOR_RETRY": "30",
 		}),
-		network.WithNetwork([]string{ /*"activator"*/ }, nw),
+		network.WithNetwork([]string{}, nw),
 	}
 	return customizers
 }
