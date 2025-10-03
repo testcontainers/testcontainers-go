4dd6e185c944e5e18a43aef00ab5a9dbe560fe33
Manuel de la Peña <mdelapenya@gmail.com>
2025-10-03 07:27:56 +0200

    chore(bigtable): use Run function

--- modules/gcloud/bigtable/bigtable.go
+++ modules/gcloud/bigtable/bigtable.go
@@ -32,16 +32,12 @@ func (c *Container) URI() string {
 // Run creates an instance of the BigTable GCloud container type.
 // The URI uses the empty string as the protocol.
 func Run(ctx context.Context, img string, opts ...testcontainers.ContainerCustomizer) (*Container, error) {
-	req := testcontainers.GenericContainerRequest{
-		ContainerRequest: testcontainers.ContainerRequest{
-			Image:        img,
-			ExposedPorts: []string{"9000/tcp"},
-			WaitingFor: wait.ForAll(
-				wait.ForListeningPort("9000/tcp"),
-				wait.ForLog("running"),
-			),
-		},
-		Started: true,
+	moduleOpts := []testcontainers.ContainerCustomizer{
+		testcontainers.WithExposedPorts("9000/tcp"),
+		testcontainers.WithWaitStrategy(wait.ForAll(
+			wait.ForListeningPort("9000/tcp"),
+			wait.ForLog("running"),
+		)),
 	}
 
 	settings := defaultOptions()
@@ -51,24 +47,23 @@ func Run(ctx context.Context, img string, opts ...testcontainers.ContainerCustom
 				return nil, err
 			}
 		}
-		if err := opt.Customize(&req); err != nil {
-			return nil, err
-		}
 	}
 
-	req.Cmd = []string{
+	moduleOpts = append(moduleOpts, testcontainers.WithCmd(
 		"/bin/sh",
 		"-c",
-		"gcloud beta emulators bigtable start --host-port 0.0.0.0:9000 --project=" + settings.ProjectID,
-	}
+		"gcloud beta emulators bigtable start --host-port 0.0.0.0:9000 --project="+settings.ProjectID,
+	))
 
-	container, err := testcontainers.GenericContainer(ctx, req)
+	moduleOpts = append(moduleOpts, opts...)
+
+	ctr, err := testcontainers.Run(ctx, img, moduleOpts...)
 	var c *Container
-	if container != nil {
-		c = &Container{Container: container, settings: settings}
+	if ctr != nil {
+		c = &Container{Container: ctr, settings: settings}
 	}
 	if err != nil {
-		return c, fmt.Errorf("generic container: %w", err)
+		return c, fmt.Errorf("run bigtable: %w", err)
 	}
 
 	portEndpoint, err := c.PortEndpoint(ctx, "9000/tcp", "")
